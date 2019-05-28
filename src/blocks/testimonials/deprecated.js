4a61bd22834317e737c193824f3855a741270459
Abanob <abanob@leap13.com>
2019-05-28 17:03:37 +0200

    Padding group control options added to Testimonials block container

--- src/blocks/testimonials/deprecated.js
+++ src/blocks/testimonials/deprecated.js
@@ -6,6 +6,34 @@ const className = "premium-testimonial";
 
 const { RichText } = wp.editor;
 
+const newAttributes_1_6_5 = {
+  backColor: {
+    type: "string"
+  },
+  imageID: {
+    type: "string"
+  },
+  imageURL: {
+    type: "string"
+  },
+  backgroundRepeat: {
+    type: "string",
+    default: "no-repeat"
+  },
+  backgroundPosition: {
+    type: "string",
+    default: "top center"
+  },
+  backgroundSize: {
+    type: "string",
+    default: "auto"
+  },
+  fixed: {
+    type: "boolean",
+    default: false
+  }
+};
+
 const newAttributes_1_3_9 = {
   shadowColor: {
     type: "string"
@@ -144,7 +172,184 @@ const testimonialsAttrs_1_3_9 = Object.assign(
   newAttributes_1_3_9
 );
 
+const testimonialsAttrs_1_6_5 = Object.assign(
+  testimonialsAttrs_1_3_9,
+  newAttributes_1_6_5
+);
+
 const deprecatedContent = [
+  {
+    attributes: testimonialsAttrs_1_6_5,
+    migrate: attributes => {
+      let newAttributes = {
+        paddingTop: "",
+        paddingRight: "",
+        paddingBottom: "",
+        paddingLeft: "",
+        paddingUnit: "px"
+      };
+      return Object.assign(attributes, newAttributes);
+    },
+    save: props => {
+      const {
+        align,
+        authorImgUrl,
+        imgRadius,
+        imgBorder,
+        imgBorderColor,
+        imgSize,
+        text,
+        authorTag,
+        authorColor,
+        authorSize,
+        authorLetter,
+        authorStyle,
+        authorUpper,
+        authorWeight,
+        author,
+        authorComTag,
+        authorComColor,
+        authorComSize,
+        authorCom,
+        quotSize,
+        quotColor,
+        quotOpacity,
+        bodyColor,
+        bodySize,
+        bodyLine,
+        bodyTop,
+        bodyBottom,
+        dashColor,
+        urlCheck,
+        urlText,
+        urlTarget,
+        shadowBlur,
+        shadowColor,
+        shadowHorizontal,
+        shadowVertical,
+        shadowPosition,
+        backColor,
+        imageURL,
+        fixed,
+        backgroundRepeat,
+        backgroundPosition,
+        backgroundSize
+      } = props.attributes;
+
+      return (
+        <div
+          className={`${className}__wrap`}
+          style={{
+            boxShadow: `${shadowHorizontal}px ${shadowVertical}px ${shadowBlur}px ${shadowColor} ${shadowPosition}`,
+            backgroundColor: backColor,
+            backgroundImage: `url('${imageURL}')`,
+            backgroundRepeat: backgroundRepeat,
+            backgroundPosition: backgroundPosition,
+            backgroundSize: backgroundSize,
+            backgroundAttachment: fixed ? "fixed" : "unset"
+          }}
+        >
+          <div className={`${className}__container`}>
+            <span className={`${className}__upper`}>
+              <PremiumUpperQuote
+                size={quotSize}
+                color={quotColor}
+                opacity={quotOpacity}
+              />
+            </span>
+            <div
+              className={`${className}__content`}
+              style={{
+                textAlign: align
+              }}
+            >
+              <div className={`${className}__img_wrap`}>
+                {authorImgUrl && (
+                  <img
+                    className={`${className}__img`}
+                    src={`${authorImgUrl}`}
+                    alt="Author"
+                    style={{
+                      borderWidth: imgBorder + "px",
+                      borderRadius: imgRadius,
+                      borderColor: imgBorderColor,
+                      width: imgSize + "px",
+                      height: imgSize + "px"
+                    }}
+                  />
+                )}
+                {!authorImgUrl && <DefaultImage className={className} />}
+              </div>
+              <div className={`${className}__text_wrap`}>
+                <div>
+                  <RichText.Content
+                    tagName="p"
+                    className={`${className}__text`}
+                    value={text}
+                    style={{
+                      color: bodyColor,
+                      fontSize: bodySize + "px",
+                      lineHeight: bodyLine + "px",
+                      marginTop: bodyTop + "px",
+                      marginBottom: bodyBottom + "px"
+                    }}
+                  />
+                </div>
+              </div>
+              <div className={`${className}__info`}>
+                <RichText.Content
+                  tagName={authorTag.toLowerCase()}
+                  className={`${className}__author`}
+                  value={author}
+                  style={{
+                    color: authorColor,
+                    fontSize: authorSize + "px",
+                    letterSpacing: authorLetter + "px",
+                    textTransform: authorUpper ? "uppercase" : "none",
+                    fontStyle: authorStyle,
+                    fontWeight: authorWeight
+                  }}
+                />
+                <span
+                  className={`${className}__sep`}
+                  style={{
+                    color: dashColor
+                  }}
+                >
+                  &nbsp;-&nbsp;
+                </span>
+                <div className={`${className}__link_wrap`}>
+                  <RichText.Content
+                    tagName={authorComTag.toLowerCase()}
+                    className={`${className}__author_comp`}
+                    value={authorCom}
+                    style={{
+                      color: authorComColor,
+                      fontSize: authorComSize + "px"
+                    }}
+                  />
+                  {urlCheck && (
+                    <a
+                      rel="noopener noreferrer"
+                      href={urlText}
+                      target={urlTarget ? "_blank" : ""}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+            <span className={`${className}__lower`}>
+              <PremiumLowerQuote
+                color={quotColor}
+                size={quotSize}
+                opacity={quotOpacity}
+              />
+            </span>
+          </div>
+        </div>
+      );
+    }
+  },
   {
     attributes: testimonialsAttrs_1_3_9,
     migrate: attributes => {
