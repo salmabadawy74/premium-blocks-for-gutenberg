2a7957d5ecd79bfefd2313f854c6fc40ea3241bf
AbanobLeap13 <abanob.akram@nilecreations.com>
2020-06-14 19:07:59 +0200

    code optimizations

--- assets/js/flip-box.js
+++ assets/js/flip-box.js
@@ -8,37 +8,45 @@ jQuery(document).ready(function ($) {
     let $flipBox = $(flipBox);
     let animation = $flipBox.data('animation');
     let $flip = $flipBox.find(".premium-flip-box");
-    let frontRight = $flipBox.find(`.premium-flip-frontrl`);
-    let frontLeft = $flipBox.find(`.premium-flip-frontlr`);
-    let frontWrapper = $flipBox.find(`.premium-flip-text-wrapper`);
-    let backWrapper = $flipBox.find(`.premium-flip-back-text-wrapper`);
+    let $frontRight = $flipBox.find(`.premium-flip-frontrl`);
+    let $frontLeft = $flipBox.find(`.premium-flip-frontlr`);
+    let $frontWrapper = $flipBox.find(`.premium-flip-text-wrapper`);
+    let $backWrapper = $flipBox.find(`.premium-flip-back-text-wrapper`);
 
     $($flipBox).on('mouseenter', function () {
+
       $flip.addClass("flipped");
+
       if (!animation)
         return;
+
       // condition left now working
-      if (frontRight) {
-        frontWrapper.removeClass("PafadeInLeft").addClass("PafadeInRight").removeClass("PafadeInRevLeft");
-        backWrapper.removeClass("PafadeInRight").addClass("PafadeInLeft").removeClass("PafadeInRevRight");
-      }
-      else if (frontLeft) {
-        frontWrapper.removeClass("PafadeInRevLeft").addClass("PafadeInRevRight").removeClass("PafadeInLeft");
-        backWrapper.removeClass("PafadeInRevRight").addClass("PafadeInRevLeft").removeClass("PafadeInRight");
+      if ($frontRight.length) {
+        console.log($frontRight);
+        $frontWrapper.removeClass("PafadeInLeft").addClass("PafadeInRight");
+        $backWrapper.removeClass("PafadeInRight").addClass("PafadeInLeft");
+      } else if ($frontLeft.length) {
+        console.log($frontLeft);
+        $frontWrapper.removeClass("PafadeInRevLeft").addClass("PafadeInRevRight");
+        $backWrapper.removeClass("PafadeInRevRight").addClass("PafadeInRevLeft");
       }
     });
 
     $($flipBox).on('mouseleave', function () {
+
       $flip.removeClass("flipped");
+
       if (!animation)
         return;
-      if (frontRight) {
-        frontWrapper.addClass("PafadeInLeft").removeClass("PafadeInRight").removeClass("PafadeInRevRight");
-        backWrapper.addClass("PafadeInRight").removeClass("PafadeInLeft").removeClass("PafadeInRevLeft");
-      } else if (frontLeft) {
-        frontWrapper.addClass("PafadeInRevLeft").removeClass("PafadeInRevRight").removeClass("PafadeInRight");
-        backWrapper.addClass("PafadeInRevRight").removeClass("PafadeInRevLeft").removeClass("PafadeInLeft");
+
+      if ($frontRight.length) {
+        $frontWrapper.addClass("PafadeInLeft").removeClass("PafadeInRight");
+        $backWrapper.addClass("PafadeInRight").removeClass("PafadeInLeft");
+      } else if ($frontLeft.length) {
+        $frontWrapper.addClass("PafadeInRevLeft").removeClass("PafadeInRevRight");
+        $backWrapper.addClass("PafadeInRevRight").removeClass("PafadeInRevLeft");
       }
+
     })
 
   });
