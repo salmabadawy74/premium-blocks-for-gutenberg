b6e34c4287d7bdc9796ab82760a0c59b72aae02d
ranaa96 <rana@leap13.com>
2021-01-27 17:25:00 +0200

    added block js files

---
+++ src/blocks/image-accordion/edit.js
@@ -0,0 +1,158 @@
+import PremiumBorder from "../../components/premium-border";
+import PremiumBoxShadow from "../../components/premium-box-shadow";
+import PremiumFilters from "../../components/premium-filters";
+import PremiumSizeUnits from "../../components/premium-size-units";
+import PremiumTypo from "../../components/premium-typo";
+import PremiumMarginR from "../../components/premium-margin-responsive";
+import PremiumRange from "../../components/premium-range-responsive";
+const { __ } = wp.i18n;
+const { Component, Fragment } = wp.element;
+const {
+  PanelBody,
+  RangeControl,
+  SelectControl,
+  Spinner,
+  ToggleControl,
+  TabPanel,
+  Dashicon,
+  TextControl,
+    IconButton,
+  Button
+} = wp.components;
+const {   InspectorControls,
+
+    ColorPalette,
+  
+    MediaUpload } = wp.editor;
+const { withSelect, select } = wp.data;
+class edit extends Component
+{
+    constructor ()
+    {
+        super( ...arguments );
+    }
+    componentDidMount ()
+    {
+        this.props.setAttributes( { block_id: this.props.clientId } );
+        this.props.setAttributes( { classMigrate: true } );
+        const $style = document.createElement( "style" );
+        $style.setAttribute( "id", "premium-post-style-" + this.props.clientId );
+        document.head.appendChild( $style );
+    }
+    render ()
+    {
+        const {
+            setAttributes,
+      
+            isSelected,
+            clientId: blockID,
+        } = this.props;
+        const { locations,block_id } = this.props.attributes;
+        const positionOptions = [
+            { label: 'Center Center', value: 'center center' },
+            { label: 'Center Left', value: 'center left' },
+            { label: 'Center Right', value: 'center right' },
+            { label: 'Top Center', value: 'top center' },
+            { label: 'Top Left', value: 'top left' },
+            { label: 'Top Right', value: 'top right' },
+            { label: 'Bottom Center', value: 'bottom center' },
+            { label: 'Bottom Left', value: 'bottom left' },
+            { label: 'Bottom Right', value: 'bottom right' },
+        ];
+        const sizeOptions = [
+            { label: 'Auto', value: 'auto' },
+            { label: 'Contain', value: 'contain' },
+            { label: 'Cover', value: 'cover' },
+            { label: 'Custom', value: 'custom' },
+        ];
+        const repeatOptions = [
+            { label: 'Repeat', value: 'repeat' },
+            { label: 'No-repeat', value: 'no-repeat' },
+            { label: 'Repeat-x', value: 'repeat-x' },
+            { label: 'Repeat-y', value: 'repeat-y' },
+        ];
+        const onFileSelect = (img) => {
+            setAttributes({
+              imageURL: img.url,
+              imageAlt: img.alt
+            });
+          };
+      
+            const handleAddLocation = () => {
+                const locations = [ locations ];
+                locations.push( {
+                    address: '',
+                } );
+                setAttributes( { locations } );
+            };
+    
+            const handleRemoveLocation = ( index ) => {
+                const locations = [ locations ];
+                locations.splice( index, 1 );
+                setAttributes( { locations } );
+            };
+    
+            const handleLocationChange = ( address, index ) => {
+                const locations = [ locations ];
+                locations[ index ].address = address;
+                setAttributes( { locations } );
+            };
+           
+            let locationFields,
+			locationDisplay;
+
+		if ( this.props.attributes.locations.length ) {
+			locationFields = locations.map( ( location, index ) => {
+				return <Fragment key={ index }>
+                   <MediaUpload
+                onSelect={onFileSelect}
+                value={locations.imageID}
+                render={({ open }) => (
+                <IconButton
+                  className="premium-media-uplpad-btn"
+                  label={__("Change Image")}
+                  icon="edit"
+                  onClick={open}
+                >
+                  {__("Change Image")}
+                </IconButton>
+              )}
+            />
+					<IconButton
+						className="grf__remove-location-address"
+						icon="no-alt"
+						label="Delete location"
+						onClick={ () => handleRemoveLocation( index ) }
+					/>
+				</Fragment>;
+			} );
+
+			locationDisplay = locations.map( ( location, index ) => {
+				return <img
+                id={`premium-image-${block_id}`}
+                className={`premium-image-accordion`}
+                alt={locations.imageAlt}
+                src={locations.imageURL}
+              />;
+			} );
+		}
+
+        return [
+            <InspectorControls key="1">
+                <PanelBody title={ __( 'images' ) }>
+                    { locationFields }
+                    <Button
+                        isDefault
+                        onClick={handleAddLocation }
+                    >
+                        { __( 'Add Location' ) }
+                    </Button>
+                </PanelBody>
+            </InspectorControls>,
+            <div key="2" >
+              {locationDisplay}
+            </div>
+        ];
+    }
+}
+export default edit;
---
+++ src/blocks/image-accordion/index.js
@@ -0,0 +1,118 @@
+import PbgIcon from "../icons";
+
+import edit from "./edit";
+import save from "./save";
+const { registerBlockType } = wp.blocks;
+const {
+	__,
+} = wp.i18n;
+
+registerBlockType("premium/image-accodion", {
+  title: __("Image Accordion"),
+  
+    category: "premium-blocks",
+    keywords:['Image Accordion'],
+  attributes: {
+      block_id: {
+          type: "string",
+      },
+	    locations: {
+			type: 'array',
+        default: [],
+        query: {
+          imageURl: {
+            source: "attribute",
+            selector: "img",
+            attribute: "src"
+          },
+          content: {
+            type: "string",
+          },
+          imageID: {
+            type: "number",
+          },
+          title: {
+            type: "string",
+            source: "html",
+            selector: "h2",
+          },
+          background: {
+            type: "string",
+          },
+          blur: {
+            type: "number",
+            default: "0",
+          },
+          bright: {
+            type: "number",
+            default: "100",
+          },
+          contrast: {
+            type: "number",
+            default: "100",
+          },
+          saturation: {
+            type: "number",
+            default: "100",
+          },
+          imageID: {
+            type: "number",
+          },
+          hue: {
+            type: "number",
+            default: "0",
+          },
+          borderType: {
+            type: "string",
+            default: "none",
+          },
+          borderWidth: {
+            type: "number",
+            default: "1",
+          },
+          borderRadius: {
+            type: "number",
+            default: "0",
+          },
+          borderColor: {
+            type: "string",
+          },
+          containerShadowColor: {
+            type: "string",
+          },
+          containerShadowBlur: {
+            type: "number",
+            default: "0",
+          },
+          containerShadowHorizontal: {
+            type: "number",
+            default: "0",
+          },
+          containerShadowVertical: {
+            type: "number",
+            default: "0",
+          },
+          containerShadowPosition: {
+            type: "string",
+            default: "",
+          },
+          urlCheck: {
+            type: "boolean",
+            default: false,
+          },
+          url: {
+            type: "string",
+            source: "attribute",
+            attribute: "href",
+          },
+          target: {
+            type: "boolean",
+            default: false,
+          },
+        }
+	    },
+  },
+  example: {},
+  edit: edit,
+ save:()=>{}
+});
