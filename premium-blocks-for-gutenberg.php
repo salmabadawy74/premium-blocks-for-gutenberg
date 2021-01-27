86680e6cb097095fee23414499b868b956b1ea78
ranaa96 <rana@leap13.com>
2021-01-27 18:18:46 +0200

    test commit removed

--- premium-blocks-for-gutenberg.php
+++ premium-blocks-for-gutenberg.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://premiumblocks.io/
  * Author: Leap13
  * Author URI: https://leap13.com/
- * Version: 1.8.0
+ * Version: 1.8.1
  * Text Domain: premium-blocks-for-gutenberg
  * Domain Path: /languages
  * License: GNU General Public License v3.0
