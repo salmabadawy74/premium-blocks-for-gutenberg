6639cd6285031498f2702a3422392174133c0943
ranaa96 <rana@leap13.com>
2021-02-03 17:07:59 +0200

    fix save function

--- src/blocks/image-accordion/save.js
+++ src/blocks/image-accordion/save.js
@@ -1,208 +1,300 @@
-import classnames from "classnames"
+export default function save ( props )
+{
+  
+  const { attributes } = props;
 
-export default function save ( props ) {
-    
-    const { attributes, className } = props
-    const {
-        block_id,
-        align,
-        repeaterImageAccordion,
-        direction,
-        skew,
-        height,
-        heightU,
-        contentPosition,
-        alignPosition,
-        hideDesc,
-        hoverIndex,
-        overlayColor,
-        overlayHover,
-        blur,
-        bright,
-        contrast,
-        saturation,
-        hue,
-        iconU,
-        iconSize,
-        iconColor,
-        iconHoverColor,
-        backgroundColor,
-        backgroundHover,
-        iconShadowColor,
-        iconShadowBlur,
-        iconShadowHorizontal,
-        iconShadowVertical,
-        iconShadowPosition,
-        iconborderType,
-        iconborderWidth,
-        iconborderColor,
-        iconborderRadius,
-        iconmarginTop,
-        iconmarginRight,
-        iconmarginBottom,
-        iconmarginLeft,
-        iconpaddingUnit,
-        iconpaddingTop,
-        iconpaddingRight,
-        iconpaddingBottom,
-        iconpaddingLeft,
-        titleColor,
-        titleSize,
-        titleWeight,
-        titleStyle,
-        titleLetter,
-        titleUpper,
-        titleShadowColor,
-        titleShadowBlur,
-        titleShadowHorizontal,
-        titleShadowVertical,
-        titlemarginTop,
-        titlemarginRight,
-        titlemarginBottom,
-        titlemarginLeft,
-        titlemarginUnit,
-        titlepaddingUnit,
-        titlepaddingTop,
-        titlepaddingRight,
-        titlepaddingBottom,
-        titlepaddingLeft,
-        descColor,
-        descSize,
-        descWeight,
-        descStyle,
-        descLetter,
-        descUpper,
-        descShadowColor,
-        descShadowBlur,
-        descShadowHorizontal,
-        descShadowVertical,
-        descmarginTop,
-        descmarginRight,
-        descmarginBottom,
-        descmarginLeft,
-        descpaddingUnit,
-        descpaddingTop,
-        descpaddingRight,
-        descpaddingBottom,
-        descpaddingLeft,
-        linkColor,
-        linkHover,
-        linkSize,
-        linkWeight,
-        linkStyle,
-        linkLetter,
-        linkUpper,
-        containerborderType,
-        containerborderWidth,
-        containerborderRadius,
-        containerborderColor,
-        containerShadowColor,
-        containerShadowBlur,
-        containerShadowHorizontal,
-        containerShadowVertical,
-        containerShadowPosition,
-        containermarginTop,
-        containermarginRight,
-        containermarginBottom,
-        containermarginLeft,
-        containermarginUnit,
+  const {   
+      block_id,
+      repeaterImageAccordion,
+      direction,
+      skew,
+      skewDirection,
+      height,
+      heightU,
+      contentPosition,
+      alignPosition,
+      hoverIndex,
+      overlayColor,
+      overlayHover,
+      blur,
+      bright,
+      contrast,
+      saturation,
+      hue,
+      iconU,
+      iconSize,
+      iconColor,
+      iconHoverColor,
+      iconbackgroundColor,
+      backgroundHover,
+      iconShadowColor,
+      iconShadowBlur,
+      iconShadowHorizontal,
+      iconShadowVertical,
+      iconShadowPosition,
+      iconborderType,
+      iconborderWidth,
+      iconborderColor,
+      iconborderRadius,
+      iconmarginTop,
+      iconmarginRight,
+      iconmarginBottom,
+      iconmarginLeft,
+      iconpaddingUnit,
+      iconpaddingTop,
+      iconpaddingRight,
+      iconpaddingBottom,
+      iconpaddingLeft,
+      titleColor,
+      titleSize,
+      titleWeight,
+      titleStyle,
+      titleLetter,
+      titleUpper,
+      titleShadowColor,
+      titleShadowBlur,
+      titleShadowHorizontal,
+      titleShadowVertical,
+      titlemarginTop,
+      titlemarginRight,
+      titlemarginBottom,
+      titlemarginLeft,
+      titlepaddingUnit,
+      titlepaddingTop,
+      titlepaddingRight,
+      titlepaddingBottom,
+      titlepaddingLeft,
+      descColor,
+      descSize,
+      descWeight,
+      descStyle,
+      descLetter,
+      descUpper,
+      descShadowColor,
+      descShadowBlur,
+      descShadowHorizontal,
+      descShadowVertical,
+      descmarginTop,
+      descmarginRight,
+      descmarginBottom,
+      descmarginLeft,
+      descpaddingUnit,
+      descpaddingTop,
+      descpaddingRight,
+      descpaddingBottom,
+      descpaddingLeft,
+      linkColor,
+      linkHover,
+      linkSize,
+      linkWeight,
+      linkStyle,
+      linkLetter,
+      linkUpper,
+      containerborderType,
+      containerborderWidth,
+      containerborderRadius,
+      containerborderColor,
+      containerShadowColor,
+      containerShadowBlur,
+      containerShadowHorizontal,
+      containerShadowVertical,
+      containerShadowPosition,
+      containermarginTop,
+      containermarginRight,
+      containermarginBottom,
+      containermarginLeft,
     } = attributes;
-    return (
-        <div className="premium-accordion-container" >
-        <div
-          id={`premium-accordion-section-${block_id} `}
-          className={ `premium-accordion-section` }
-          style={{
-            border: containerborderType,
-            borderWidth: containerborderWidth + "px",
-            borderRadius: containerborderRadius + "px",
-            borderColor: containerborderColor,
-            boxShadow: `${ containerShadowHorizontal }px ${ containerShadowVertical }px ${ containerShadowBlur }px ${ containerShadowColor } ${ containerShadowPosition }`,
-            marginTop: containermarginTop + containermarginUnit,
-            marginBottom: containermarginBottom + containermarginUnit,
-            marginRight: containermarginRight + containermarginUnit,
-            marginLeft: containermarginLeft + containermarginUnit,
-          }}
+
+  const skewClass = skew ? "premium-accordion-skew" : null;
+
+  return (
+    <div className="premium-accordion-container">
+    <div
+      id={`premium-accordion-section-${block_id}`}
+      className={`premium-accordion-section ${skewClass}`}
+      data-skew={skew ? skewDirection : null}
+      style={{
+        border: containerborderType,
+        borderWidth: containerborderWidth + "px",
+        borderRadius: containerborderRadius + "px",
+        borderColor: containerborderColor,
+        boxShadow: `${containerShadowHorizontal}px ${containerShadowVertical}px ${containerShadowBlur}px ${containerShadowColor} ${containerShadowPosition}`,
+        marginTop: containermarginTop + "px",
+        marginBottom: containermarginBottom + "px",
+        marginRight: containermarginRight + "px",
+        marginLeft: containermarginLeft + "px",
+      }}
+    >
+      <div className={`premium-accordion-${direction}`}>
+        <ul
+          className={`premium-accordion-ul premium-accordion-${contentPosition}`}
         >
-          <div className={`premium-accordion-${direction}`}>
-            <ul className={`premium-accordion-ul premium-accordion-center`}>
-              {repeaterImageAccordion.map((item, index) => {
-                return (
-                  <li className={ `premium-accordion-li` }
-                    id={`premium-accordion-li-${index}`}
-                    style={ {
-                    height: height + heightU,
-                    filter:`brightness( ${bright}% ) contrast( ${contrast}% ) saturate( ${saturation}% ) blur( ${blur}px ) hue-rotate( ${hue}deg )`,
-                    
-                  }}>
-                    <div className={ `premium-accordion-background` } ></div>
-                    <a className={ `premium-accordion-item-link` } title={ item.urlTitle } href={ item.url } style={ {
-                      color: linkColor,
-                      fontSize: linkSize + 'px',
-                      fontWeight: linkWeight,
-                      fontStyle: linkStyle,
-                      letterSpacing:linkLetter
-                    }}>{item.urlTitle }</a>
-                    <div className={ `premium-accordion-overlay-wrap` }
-                    style={{alignItems:contentPosition,justifyContent:alignPosition,backgroundColor:backgroundColor}}
+          {repeaterImageAccordion.map((item, index) => {
+            return (
+              <li
+                className={`premium-accordion-li ${
+                  index === hoverIndex
+                    ? "premium-accordion-li-active"
+                    : null
+                }`}
+                id={`premium-accordion-li-${index}`}
+                style={{
+                  height: height + heightU,
+                  filter: `brightness( ${bright}% ) contrast( ${contrast}% ) saturate( ${saturation}% ) blur( ${blur}px ) hue-rotate( ${hue}deg )`,
+                }}
+              >
+                <div className={`premium-accordion-background`}></div>
+
+                <div
+                  className={`premium-accordion-overlay-wrap`}
+                  style={{
+                    alignItems: contentPosition,
+                    justifyContent: alignPosition,
+                  }}
+                >
+                  {item.content && (
+                    <div
+                      className={`premium-accordion-content premium-accordion-${alignPosition} ${
+                        item.custom ? "custom-content" : null
+                      }`}
                     >
-                      <div
-                        className={`accpremium-accordion-content premium-accordion-${contentPosition}`}
+                      {item.icon && (
+                        <i
+                          className={`premium-accordion-icon ${item.selectedIcon}`}
+                          style={{
+                            fontStyle: "normal",
+                            fontSize: iconSize + iconU,
+                            boxShadow: `${iconShadowHorizontal}px ${iconShadowVertical}px ${iconShadowBlur}px ${iconShadowColor} ${iconShadowPosition}`,
+                            border: iconborderType,
+                            borderWidth: iconborderWidth + "px",
+                            borderRadius: iconborderRadius + "px",
+                            borderColor: iconborderColor,
+                            marginTop: iconmarginTop + "px",
+                            marginBottom: iconmarginBottom + "px",
+                            marginRight: iconmarginRight + "px",
+                            marginLeft: iconmarginLeft + "px",
+                            paddingTop: iconpaddingTop + iconpaddingUnit,
+                            paddingBottom:
+                              iconpaddingBottom + iconpaddingUnit,
+                            paddingRight:
+                              iconpaddingRight + iconpaddingUnit,
+                            paddingLeft: iconpaddingLeft + iconpaddingUnit,
+                          }}
+                        ></i>
+                      )}
+                      <h3
+                        className={`premium-accordion-title`}
+                        style={{
+                          fontSize: titleSize + "px",
+                          color: titleColor,
+                          fontWeight: titleWeight,
+                          letterSpacing: titleLetter,
+                          textTransform: titleUpper ? "uppercase" : null,
+                          fontStyle: titleStyle,
+                          marginTop: titlemarginTop + "px",
+                          marginBottom: titlemarginBottom + "px",
+                          marginRight: titlemarginRight + "px",
+                          marginLeft: titlemarginLeft + "px",
+                          paddingTop: titlepaddingTop + titlepaddingUnit,
+                          paddingBottom:
+                            titlepaddingBottom + titlepaddingUnit,
+                          paddingRight:
+                            titlepaddingRight + titlepaddingUnit,
+                          paddingLeft: titlepaddingLeft + titlepaddingUnit,
+                          textShadow: `${titleShadowHorizontal}px ${titleShadowVertical}px ${titleShadowBlur}px ${titleShadowColor}`,
+                        }}
                       >
-                        <h3 className={ `premium-accordion-title` }
-                          style={ {
-                            font: titleSize + 'px', color: titleColor, fontWeight: titleWeight, letterSpacing: titleLetter, fontStyle: titleStyle,
-                            marginTop: titlemarginTop +'px',
-                            marginBottom: titlemarginBottom + 'px',
-                            marginRight: titlemarginRight + 'px',
-                            marginLeft: titlemarginLeft + 'px',
-                           paddingTop: titlepaddingTop + titlepaddingUnit,
-                           paddingBottom: titlepaddingBottom + titlepaddingUnit,
-                           paddingRight: titlepaddingRight + titlepaddingUnit,
-                            paddingLeft: titlepaddingLeft + titlepaddingUnit,
-                           textShadow:`${ titleShadowHorizontal }px ${ titleShadowVertical }px ${ titleShadowBlur }px ${ titleShadowColor }`
-                          } }
-                        >{ item.title }</h3>
-                        <div className={ `premium-accordion-description` }
-                         style={ {
-                          font: descSize + 'px', color: descColor, fontWeight: descWeight, letterSpacing: descLetter, fontStyle: descStyle,
-                          marginTop: descmarginTop +'px',
-                          marginBottom: descmarginBottom + 'px',
-                          marginRight: descmarginRight + 'px',
-                          marginLeft: descmarginLeft + 'px',
-                         paddingTop: descpaddingTop + descpaddingUnit,
-                         paddingBottom: descpaddingBottom + descpaddingUnit,
-                         paddingRight: descpaddingRight + descpaddingUnit,
+                        {item.title}
+                      </h3>
+                      <div
+                        className={`premium-accordion-description`}
+                        style={{
+                          fontSize: descSize + "px",
+                          color: descColor,
+                          fontWeight: descWeight,
+                          letterSpacing: descLetter,
+                          fontStyle: descStyle,
+                          textTransform: descUpper ? "uppercase" : null,
+                          marginTop: descmarginTop + "px",
+                          marginBottom: descmarginBottom + "px",
+                          marginRight: descmarginRight + "px",
+                          marginLeft: descmarginLeft + "px",
+                          paddingTop: descpaddingTop + descpaddingUnit,
+                          paddingBottom:
+                            descpaddingBottom + descpaddingUnit,
+                          paddingRight: descpaddingRight + descpaddingUnit,
                           paddingLeft: descpaddingLeft + descpaddingUnit,
-                         textShadow:`${ descShadowHorizontal }px ${ descShadowVertical }px ${ descShadowBlur }px ${ descShadowColor }`
-                        } }
-                        >{ item.desc }</div>
+                          textShadow: `${descShadowHorizontal}px ${descShadowVertical}px ${descShadowBlur}px ${descShadowColor}`,
+                        }}
+                      >
+                        {item.desc}
                       </div>
+                      {item.link && (
+                        <a
+                          className={`${
+                            item.whole
+                              ? "premium-accordion-item-link"
+                              : "premium-accordion-item-link-title"
+                          }`}
+                          title={item.urlTitle}
+                          href={item.url}
+                          style={{
+                            fontSize: linkSize + "px",
+                            fontWeight: linkWeight,
+                            fontStyle: linkStyle,
+                            letterSpacing: linkLetter,
+                            textTransform: linkUpper ? "uppercase" : null,
+                          }}
+                        >
+                          {item.whole ? " " : item.urlTitle}
+                        </a>
+                      )}
                     </div>
-                    <style
-                      dangerouslySetInnerHTML={ {
-                        __html: [
-                          ` #premium-accordion-li-${index} .premium-accordion-background{`,
-                          `background-image:url(${ item.imageURL });`,
-                          `background-size : ${ item.backgroundSize };`,
-                          `background-position : ${ item.backgroundPosition };`,
-                          `background-repeat : ${item.backgroundRepeat};`,
-                          "}"
-                        ].join ("\n" ),
-                      } }
-                    />
-                  </li>
-                 
-                );
-                
-
-
-               
-              })}
-            </ul>
-          </div>
-        </div>
+                  )}
+                </div>
+                <style
+                  dangerouslySetInnerHTML={{
+                    __html: [
+                      `#premium-accordion-section-${block_id} #premium-accordion-li-${index} .premium-accordion-background{`,
+                      `background-image:url(${item.imageURL});`,
+                      `background-size : ${item.backgroundSize};`,
+                      `background-position : ${item.backgroundPosition};`,
+                      `background-repeat : ${item.backgroundRepeat};`,
+                      "}",
+                      `#premium-accordion-section-${block_id} #premium-accordion-li-${index} .custom-content{`,
+                      `position: absolute;`,
+                      `top:${item.horizontal}${item.horizontalU};`,
+                      `left:${item.vertical}${item.verticalU};`,
+                      "}",
+                      `.premium-accordion-section .premium-accordion-item-link {`,
+                      `color: ${linkColor};`,
+                      "}",
+                      `.premium-accordion-section:hover .premium-accordion-item-link {`,
+                      `color: ${linkHover};`,
+                      "}",
+                      ".premium-accordion-section .premium-accordion-overlay-wrap{",
+                      `background-color: ${overlayColor};`,
+                      "}",
+                      `.premium-accordion-section:hover .premium-accordion-overlay-wrap {`,
+                      `background-color: ${overlayHover};`,
+                      "}",
+                      ` .premium-accordion-section .premium-accordion-icon{`,
+                      `color:${iconColor};`,
+                      `background-color :${iconbackgroundColor};`,
+                      "}",
+                      ` .premium-accordion-section:hover .premium-accordion-icon{`,
+                      `color:${iconHoverColor};`,
+                      `background-color :${backgroundHover};`,
+                      "}",
+                    ].join("\n"),
+                  }}
+                />
+              </li>
+            );
+          })}
+        </ul>
       </div>
-   
-    )
+    </div>
+  </div>
+  );
 }
