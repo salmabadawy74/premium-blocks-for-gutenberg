b6abe336f487ef604bc488d5c78bbfec53bb6c2c
ranaa96 <rana@leap13.com>
2021-02-10 12:51:36 +0200

    fix tag name

--- src/blocks/blog/Tags.js
+++ src/blocks/blog/Tags.js
@@ -2,21 +2,19 @@ const { __ } = wp.i18n;
 class Tags extends React.Component {
   render() {
     const { post, attributes, tagList } = this.props;
-    let tagObject = [];
+    let tagName = [];
     if (tagList) {
       tagList.map((tag, thisIndex) => {
         if (tag.id == post.tags[thisIndex]) {
-          tagObject.push(tag);
+          tagName.push(tag.name);
         }
       });
     }
-    if (tagObject.length > 0 && attributes.displayPostTags) {
+    if (tagName.length > 0 && attributes.displayPostTags) {
       return (
         <div className={`premium-blog-post-tags-container`}>
           <i className={`fa fa-tags fa-fw`}></i>
-          {tagObject.map((tag) => (
-            <span>{tag.name + " ,  " }</span>
-          ))}
+        {tagName.join(',')}
         </div>
       );
     } else {
