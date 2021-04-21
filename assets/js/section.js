349822edd64e36c1c5b3ca8bf20a93fb17e34ac5
Abanob <abanob@leap13.com>
2021-04-21 18:13:29 +0200

    code formatting

--- assets/js/section.js
+++ assets/js/section.js
@@ -1,84 +1,84 @@
-(function($) {
-  const { isRTL } = siteDirection;
-  let isEditMode;
+(function ($) {
+    const { isRTL } = siteDirection;
+    let isEditMode;
 
-  const hideToolbar = () => {
-    let $section = $(".premium-container"),
-      isStretched = $section.hasClass("premium-container__stretch_true");
-    let $toolbar = $section
-      .closest("div[data-type='premium/container']")
-      .find(".editor-block-list__block-edit")
-      .eq(0)
-      .find(".components-toolbar")
-      .eq(1);
+    const hideToolbar = () => {
+        let $section = $(".premium-container"),
+            isStretched = $section.hasClass("premium-container__stretch_true");
+        let $toolbar = $section
+            .closest("div[data-type='premium/container']")
+            .find(".editor-block-list__block-edit")
+            .eq(0)
+            .find(".components-toolbar")
+            .eq(1);
 
-    if (isStretched) {
-      $toolbar.css("display", "none");
-    } else {
-      $toolbar.css("display", "flex");
-    }
-  };
-  const stretchSection = () => {
-    const $stretchedSections = $(".premium-container");
-    $stretchedSections.map((index, section) => {
-      let $section = $(section),
-        isStretched = $section.hasClass("premium-container__stretch_true"),
-        css = {};
-      if (isStretched) {
-        let width = $(window).outerWidth(),
-          offsetLeft = !isEditMode
-            ? $section.offset().left
-            : $section.offset().left - $("#adminmenuwrap").outerWidth(),
-          isFixed = "fixed" === $section.css("position"),
-          correctOffset = isFixed ? 0 : offsetLeft;
-
-        if (isEditMode) {
-          width = $(".edit-post-layout__content").outerWidth();
+        if (isStretched) {
+            $toolbar.css("display", "none");
+        } else {
+            $toolbar.css("display", "flex");
         }
-        if (!isFixed) {
-          if (isRTL) {
-            correctOffset = width - ($section.outerWidth() + correctOffset);
-          }
+    };
+    const stretchSection = () => {
+        const $stretchedSections = $(".premium-container");
+        $stretchedSections.map((index, section) => {
+            let $section = $(section),
+                isStretched = $section.hasClass("premium-container__stretch_true"),
+                css = {};
+            if (isStretched) {
+                let width = $(window).outerWidth(),
+                    offsetLeft = !isEditMode
+                        ? $section.offset().left
+                        : $section.offset().left - $("#adminmenuwrap").outerWidth(),
+                    isFixed = "fixed" === $section.css("position"),
+                    correctOffset = isFixed ? 0 : offsetLeft;
 
-          correctOffset = -correctOffset;
-        }
+                if (isEditMode) {
+                    width = $(".edit-post-layout__content").outerWidth();
+                }
+                if (!isFixed) {
+                    if (isRTL) {
+                        correctOffset = width - ($section.outerWidth() + correctOffset);
+                    }
 
-        css.width = width + "px";
+                    correctOffset = -correctOffset;
+                }
 
-        let direction = isRTL ? "right" : "left";
+                css.width = width + "px";
 
-        css[direction] = correctOffset + "px";
+                let direction = isRTL ? "right" : "left";
 
-        $section.css(css);
-      } else {
-        css.width = "100%";
+                css[direction] = correctOffset + "px";
 
-        let direction = isRTL ? "right" : "left";
+                $section.css(css);
+            } else {
+                css.width = "100%";
 
-        css[direction] = "0px";
+                let direction = isRTL ? "right" : "left";
 
-        $section.css(css);
-      }
+                css[direction] = "0px";
+
+                $section.css(css);
+            }
+        });
+    };
+
+    jQuery(document).ready(() => {
+        setTimeout(() => {
+            isEditMode = $(".edit-post-layout__content").length;
+            stretchSection();
+            hideToolbar();
+            $(".premium-container").on("click", e => {
+                let $checkBox = $(".components-form-toggle__input");
+                hideToolbar();
+                if ($checkBox.length) {
+                    $checkBox.on("click", () => {
+                        setTimeout(() => {
+                            stretchSection();
+                            hideToolbar();
+                        }, 500);
+                    });
+                }
+            });
+        }, 1000);
     });
-  };
-
-  jQuery(document).ready(() => {
-    setTimeout(() => {
-      isEditMode = $(".edit-post-layout__content").length;
-      stretchSection();
-      hideToolbar();
-      $(".premium-container").on("click", e => {
-        let $checkBox = $(".components-form-toggle__input");
-        hideToolbar();
-        if ($checkBox.length) {
-          $checkBox.on("click", () => {
-            setTimeout(() => {
-              stretchSection();
-              hideToolbar();
-            }, 500);
-          });
-        }
-      });
-    }, 1000);
-  });
 })(jQuery);
