e923c0ec1638b6d22d194856630e8f6369a1021e
salmamohamed74 <salma.mohamed@leap13.com>
2022-01-18 16:41:08 +0200

    fix deprecated function

--- classes/class-pbg-blocks-helper.php
+++ classes/class-pbg-blocks-helper.php
@@ -733,7 +733,6 @@ class PBG_Blocks_Helper {
 		$media_query['tablet']  = apply_filters( 'Premium_BLocks_tablet_media_query', '(max-width: 1024px)' );
 		$media_query['desktop'] = apply_filters( 'Premium_BLocks_tablet_media_query', '(min-width: 1025px)' );
 		// FancyText Style
-
 		if ( isset( $attr['fancyStyles'] ) ) {
 			if ( isset( $attr['fancyStyles'][0]['fancyTextfontSize'] ) ) {
 				$css->set_selector( '#premium-fancy-text-' . $unique_id . '> .premium-fancy-text-title' );
