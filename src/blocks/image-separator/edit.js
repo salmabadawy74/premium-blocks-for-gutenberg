52ec3a70191fd0e014608f33f83ec085e37be5d1
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-03-21 08:26:43 +0200

    fix attribute name

--- src/blocks/image-separator/edit.js
+++ src/blocks/image-separator/edit.js
@@ -223,13 +223,9 @@ class edit extends Component {
                                         "premium-blocks-for-gutenberg"
                                     )}
                                     value={imgHeight}
-<<<<<<< HEAD
                                     onChange={(value) =>
-                                        setAttributes({ imgHeighte: value })
+                                        setAttributes({ imgHeight: value })
                                     }
-=======
-                                    onChange={value => setAttributes({ imgHeight: value })}
->>>>>>> a0ba0123cf1446bd5eea86a742677981fc000d48
                                     tabletValue={imgHeightTablet}
                                     onChangeTablet={(value) =>
                                         setAttributes({
