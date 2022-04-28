aab1cb44879e2b56464318afef872dcf039f7bbe
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-04-28 15:33:49 +0200

    fix issue of title styles in save

--- src/blocks/heading/save.js
+++ src/blocks/heading/save.js
@@ -220,7 +220,6 @@ export default function save(props) {
                             'data-shiny-dur': `${titleStyles[0].animateduration}`,
                             style: {
                                 color: titleStyles[0].titleColor,
-                                // fontSize: TitleSize + titleStyles[0].titlefontSizeType,
                                 fontWeight: titleStyles[0].titleWeight,
                                 letterSpacing: titleStyles[0].titleLetter + "px",
                                 lineHeight: (titleStyles[0].titleLine ? titleStyles[0].titleLine : "") + "px",
@@ -231,110 +230,58 @@ export default function save(props) {
                             }
                         },
                         [
-                            style === 'style7' ?
-                                <Fragment>
-                                    {
-                                        iconPosition != 'top' && iconValue &&
-                                        <span className={`premium-title-style7-stripe__wrap premium-stripe-${stripePosition} premium-stripe-${stripeAlign}`}>
-                                            <span className={`premium-title-style7-stripe-span`}></span>
-                                        </span>
-                                    }
-                                    {
-                                        !iconValue &&
-                                        <span className={`premium-title-style7-stripe__wrap premium-stripe-${stripePosition} premium-stripe-${stripeAlign}`}>
-                                            <span className={`premium-title-style7-stripe-span`}></span>
-                                        </span>
-                                    }
-                                    <div className={`premium-title-style7-inner-title`}>
+                            <Fragment>
+                                {style === 'style7' ?
+                                    <Fragment>
                                         {
-                                            iconValue && iconType == 'icon' && <i className={`premium-title-icon ${icon}`} />
-                                        }
-                                        {
-                                            iconValue && iconType == 'image' && < img className={`premium-title-icon`} src={imageURL} />
-                                        }
-                                        {
-                                            iconValue && iconType == 'lottie' &&
-                                            <div
-                                                className="premium-title-icon premium-lottie-animation"
-                                                data-loop={loop}
-                                                data-lottieurl={`${lottieURl}`}
-                                                data-reverse={reversedir}
-                                                data-trigger={"none"}
-                                            >
-
-                                            </div>
-                                        }
-
-                                        {
-                                            iconPosition === 'top' &&
+                                            iconPosition != 'top' && iconValue &&
                                             <span className={`premium-title-style7-stripe__wrap premium-stripe-${stripePosition} premium-stripe-${stripeAlign}`}>
                                                 <span className={`premium-title-style7-stripe-span`}></span>
                                             </span>
                                         }
-                                        <RichText.Content
-                                            tagName='span'
-                                            className={`premium-title-text-title`}
-                                            value={title}
-                                            style={{
-                                                color: titleStyles[0].titleColor,
-                                                // fontSize: TitleSize + titleStyles[0].titlefontSizeType,
-                                                // fontWeight: titleStyles[0].titleWeight,
-                                                // letterSpacing: titleStyles[0].titleLetter + "px",
-                                                // lineHeight: titleStyles[0].titleLine + "px",
-                                                // fontStyle: titleStyles[0].titleStyle,
-                                                // textTransform: titleStyles[0].titleUpper ? "uppercase" : "none",
-                                                // fontFamily: titleStyles[0].titleFontFamily,
-                                                // textShadow: `${titleStyles[0].titleShadowHorizontal}px ${titleStyles[0].titleShadowVertical}px ${titleStyles[0].titleShadowBlur}px ${titleStyles[0].titleShadowColor}`,
-                                                // marginTop: TitleMarginTop + titleStyles[0].titleMarginType,
-                                                // marginBottom: TitleMarginBottom + titleStyles[0].titleMarginType,
-                                                // marginLeft: TitleMarginLeft + titleStyles[0].titleMarginType,
-                                                // marginRight: TitleMarginRight + titleStyles[0].titleMarginType,
-                                                // paddingTop: TitlePaddingTop + titleStyles[0].titlePaddingType,
-                                                // paddingBottom: TitlePaddingBottom + titleStyles[0].titlePaddingType,
-                                                // paddingLeft: TitlePaddingLeft + titleStyles[0].titlePaddingType,
-                                                // paddingRight: TitlePaddingRight + titleStyles[0].titlePaddingType,
-                                                // minHeight: '15px'
-                                            }}
-                                        />
-                                    </div>
-                                </Fragment>
-                                : style === "style9" ? <Fragment>
-                                    {
-                                        iconValue && iconType == 'icon' && <i className={`premium-title-icon ${icon}`} />
-                                    }
-                                    {
-                                        iconValue && iconType == 'image' && < img className={`premium-title-icon`} src={imageURL} />
-                                    }
-                                    {
-                                        iconValue && iconType == 'lottie' &&
-                                        <div
-                                            className="premium-title-icon premium-lottie-animation"
-                                            data-loop={loop}
-                                            data-lottieurl={`${lottieURl}`}
-                                            data-reverse={reversedir}
-                                            data-trigger={"none"}
-                                        >
+                                        {
+                                            !iconValue &&
+                                            <span className={`premium-title-style7-stripe__wrap premium-stripe-${stripePosition} premium-stripe-${stripeAlign}`}>
+                                                <span className={`premium-title-style7-stripe-span`}></span>
+                                            </span>
+                                        }
+                                        <div className={`premium-title-style7-inner-title`}>
+                                            {
+                                                iconValue && iconType == 'icon' && <i className={`premium-title-icon ${icon}`} />
+                                            }
+                                            {
+                                                iconValue && iconType == 'image' && < img className={`premium-title-icon`} src={imageURL} />
+                                            }
+                                            {
+                                                iconValue && iconType == 'lottie' &&
+                                                <div
+                                                    className="premium-title-icon premium-lottie-animation"
+                                                    data-loop={loop}
+                                                    data-lottieurl={`${lottieURl}`}
+                                                    data-reverse={reversedir}
+                                                    data-trigger={"none"}
+                                                >
 
+                                                </div>
+                                            }
+
+                                            {
+                                                iconPosition === 'top' &&
+                                                <span className={`premium-title-style7-stripe__wrap premium-stripe-${stripePosition} premium-stripe-${stripeAlign}`}>
+                                                    <span className={`premium-title-style7-stripe-span`}></span>
+                                                </span>
+                                            }
+                                            <RichText.Content
+                                                tagName='span'
+                                                className={`premium-title-text-title`}
+                                                value={title}
+                                                style={{
+                                                    color: titleStyles[0].titleColor,
+                                                }}
+                                            />
                                         </div>
-                                    }
-                                    <span
-                                        className={`premium-letters-container`}
-                                        style={{
-                                            color: titleStyles[0].titleColor,
-                                            // marginTop: TitleMarginTop + titleStyles[0].titleMarginType,
-                                            // marginBottom: TitleMarginBottom + titleStyles[0].titleMarginType,
-                                            // marginLeft: TitleMarginLeft + titleStyles[0].titleMarginType,
-                                            // marginRight: TitleMarginRight + titleStyles[0].titleMarginType,
-                                            // paddingTop: TitlePaddingTop + titleStyles[0].titlePaddingType,
-                                            // paddingBottom: TitlePaddingBottom + titleStyles[0].titlePaddingType,
-                                            // paddingLeft: TitlePaddingLeft + titleStyles[0].titlePaddingType,
-                                            // paddingRight: TitlePaddingRight + titleStyles[0].titlePaddingType,
-                                        }}
-                                    >
-                                        {styleContainer}
-                                    </span>
-
-                                </Fragment> : <Fragment>
+                                    </Fragment>
+                                    : style === "style9" ? <Fragment>
                                         {
                                             iconValue && iconType == 'icon' && <i className={`premium-title-icon ${icon}`} />
                                         }
@@ -353,17 +300,46 @@ export default function save(props) {
 
                                             </div>
                                         }
-                                        <RichText.Content
-                                            tagName='span'
-                                            className={`premium-title-text-title`}
-                                            value={title}
+                                        <span
+                                            className={`premium-letters-container`}
                                             style={{
-                                                minHeight: '15px',
-                                                margin: 'revert'
+                                                color: titleStyles[0].titleColor
                                             }}
-                                        />
-                                    </Fragment>
-                            // {link && url !== ' ' && <a rel="noopener noreferrer" target={"_self"} href="javascript:void(0)" ></a>}
+                                        >
+                                            {styleContainer}
+                                        </span>
+
+                                    </Fragment> : <Fragment>
+                                            {
+                                                iconValue && iconType == 'icon' && <i className={`premium-title-icon ${icon}`} />
+                                            }
+                                            {
+                                                iconValue && iconType == 'image' && < img className={`premium-title-icon`} src={imageURL} />
+                                            }
+                                            {
+                                                iconValue && iconType == 'lottie' &&
+                                                <div
+                                                    className="premium-title-icon premium-lottie-animation"
+                                                    data-loop={loop}
+                                                    data-lottieurl={`${lottieURl}`}
+                                                    data-reverse={reversedir}
+                                                    data-trigger={"none"}
+                                                >
+
+                                                </div>
+                                            }
+                                            <RichText.Content
+                                                tagName='span'
+                                                className={`premium-title-text-title`}
+                                                value={title}
+                                                style={{
+                                                    minHeight: '15px',
+                                                    margin: 'revert'
+                                                }}
+                                            />
+                                        </Fragment>}
+                                {link && url !== ' ' && <a rel="noopener noreferrer" target={"_self"} href={`${url}`} ></a>}
+                            </Fragment>
                         ])}
                 </div>
             </div>
