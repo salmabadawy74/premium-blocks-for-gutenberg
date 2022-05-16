e9f7771bf1c5d16263e617dc9077cb84de8db5bb
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-05-16 09:49:03 +0200

    fix issue of overflow when click on switch

--- src/blocks/content-switcher/build/block.asset.php
+++ src/blocks/content-switcher/build/block.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd7e202853f019e3875e111c5907514ee');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c4e09e0a58017cf656a21a2b10bd3457');
