9634e663e4ebb2c8c449c4b00c03fc0180aead07
salmamohamed74 <salma.mohamed@leap13.com>
2022-05-17 15:02:57 +0200

    edit function for column block

--- src/blocks/column/edit.js
+++ src/blocks/column/edit.js
@@ -3,3 +3,21 @@ import PremiumShadow from "../../components/PremiumShadow";
 import PremiumBackgroundControl from '../../components/Premium-Background-Control'
 import classnames from "classnames";
 import PremiumBorder from "../../components/premium-border";
+import InspectorTabs from '../../components/inspectorTabs';
+import InspectorTab from '../../components/inspectorTab';
+import Animation from '../../components/Animation'
+const { __ } = wp.i18n;
+const { PanelBody, ToggleControl, SelectControl, TextControl } = wp.components;
+const { Fragment } = wp.element;
+const { withSelect } = wp.data
+const edit = (props) => {
+
+}
+export default withSelect((select, props) => {
+    const { __experimentalGetPreviewDeviceType = null } = select('core/edit-post');
+    let deviceType = __experimentalGetPreviewDeviceType ? __experimentalGetPreviewDeviceType() : null;
+
+    return {
+        deviceType: deviceType
+    }
+})(edit)
