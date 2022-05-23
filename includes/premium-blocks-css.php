c7e969c46f736be102903392a5c4facde5ee694f
aaaharidy <abdelrhman@nilecreations.com>
2022-05-23 15:12:56 +0200

    modal block

--- includes/premium-blocks-css.php
+++ includes/premium-blocks-css.php
@@ -563,5 +563,12 @@ class Premium_Blocks_css {
 		// Output minified css
 		return $this->_output;
 	}
+	
+	public function get_responsive_value( $values, $side = '', $device = 'Desktop', $unit = 'px' ) {
+		return isset( $values[ $device ][ $side ] ) && $values[ $device ][ $side ] ? "{$values[ $device ][ $side ]}{$unit}" : '';
+	}
 
+	public function get_responsive_size_value( $values, $device = 'Desktop', $unit = 'px' ) {
+		return isset( $values[ $device ] ) && $values[ $device ] ? "{$values[ $device ]}{$unit}" : '';
+	}
 }
--- src/blocks/Modal/save.js
+++ src/blocks/Modal/save.js
@@ -1,5 +1,5 @@
 import classnames from 'classnames'
-
+import { gradientBackground } from '../../components/HelperFunction';
 const { InnerBlocks } = wp.blockEditor;
 
 const save = props => {
@@ -11,32 +11,30 @@ const save = props => {
         contentStyles,
         triggerSettings,
         triggerStyles,
-        triggerBorderTop,
-        triggerBorderRight,
-        triggerBorderBottom,
-        triggerBorderLeft,
+        triggerBorder,
         headerStyles,
-        headerBorderTop,
-        headerBorderRight,
-        headerBorderBottom,
-        headerBorderLeft,
+        headerBorder,
         upperStyles,
-        upperBorderTop,
-        upperBorderRight,
-        upperBorderBottom,
-        upperBorderLeft,
+        upperBorder,
         lowerStyles,
-        lowerBorderTop,
-        lowerBorderRight,
-        lowerBorderBottom,
-        lowerBorderLeft,
+        lowerBorder,
         modalStyles,
-        backgroundType,
-        modalBorderTop,
-        modalBorderRight,
-        modalBorderBottom,
-        modalBorderLeft,
+        modalBorder,
+        triggerBorderH,
+        modalBackground,
+        triggerShadow,
+        triggerTextShadow,
+        modalShadow,
+        triggerTypography,
+        headerTypography,
+        lowerTypography,
+        modalTypography,
+        iconSize,
+        imageWidth,
+        modalWidth,
+        modalHeight
     } = props.attributes;
+
     const renderCss = (
         <style>
             {`
@@ -63,32 +61,17 @@ const save = props => {
             filter: brightness( ${triggerStyles[0].brightH}% ) contrast( ${triggerStyles[0].contrastH}% ) saturate( ${triggerStyles[0].saturationH}% ) blur( ${triggerStyles[0].blurH}px ) hue-rotate( ${triggerStyles[0].hueH}deg ) !important;
             }
             #premium-modal-box-${block_id} .premium-modal-trigger-container img:hover {
-              border-style: ${triggerStyles[0].borderTypeH} !important;
-              border-color: ${triggerStyles[0].borderColorH} !important;
-              border-radius: ${triggerStyles[0].borderRadiusH}px !important;
+              border-style: ${triggerBorderH.borderType} !important;
+              border-color: ${triggerBorderH.borderColor} !important;
             }
              #premium-modal-box-${block_id} .premium-modal-trigger-container:hover .premium-modal-trigger-text {
-              border-style: ${triggerStyles[0].borderTypeH} !important;
-              border-color: ${triggerStyles[0].borderColorH} !important;
-              border-radius: ${triggerStyles[0].borderRadiusH}px !important;
+                border-style: ${triggerBorderH.borderType} !important;
+                border-color: ${triggerBorderH.borderColor} !important;
             }
 
         `}
         </style>
     );
-
-    let btnGrad, btnGrad2, btnbg;
-    if (undefined !== backgroundType && 'gradient' === backgroundType) {
-        btnGrad = ('transparent' === modalStyles[0].containerBack || undefined === modalStyles[0].containerBack ? 'rgba(255,255,255,0)' : modalStyles[0].containerBack);
-        btnGrad2 = (undefined !== modalStyles[0].gradientColorTwo && undefined !== modalStyles[0].gradientColorTwo && '' !== modalStyles[0].gradientColorTwo ? modalStyles[0].gradientColorTwo : '#777');
-        if ('radial' === modalStyles[0].gradientType) {
-            btnbg = `radial-gradient(at ${modalStyles[0].gradientPosition}, ${btnGrad} ${modalStyles[0].gradientLocationOne}%, ${btnGrad2} ${modalStyles[0].gradientLocationTwo}%)`;
-        } else if ('radial' !== modalStyles[0].gradientType) {
-            btnbg = `linear-gradient(${modalStyles[0].gradientAngle}deg, ${btnGrad} ${modalStyles[0].gradientLocationOne}%, ${btnGrad2} ${modalStyles[0].gradientLocationTwo}%)`;
-        }
-    } else {
-        btnbg = modalStyles[0].backgroundImageURL ? `url('${modalStyles[0].backgroundImageURL}')` : ''
-    }
 
     return (
         <div id={`premium-modal-box-${block_id}`} className={classnames(className, "premium-modal-box")} data-trigger={triggerSettings[0].triggerType}>
@@ -96,49 +79,40 @@ const save = props => {
             <div className={`premium-modal-trigger-container`} style={{ textAlign: triggerSettings[0].align }}>
                 {(triggerSettings[0].triggerType === "button") && <button className={` premium-modal-trigger-btn premium-button__${triggerSettings[0].btnSize} `} style={{
                     backgroundColor: triggerStyles[0].triggerBack,
-                    borderStyle: triggerStyles[0].borderType,
-                    borderTopWidth: `${triggerBorderTop}px`,
-                    borderRightWidth: `${triggerBorderRight}px`,
-                    borderBottomWidth: `${triggerBorderBottom}px`,
-                    borderLeftWidth: `${triggerBorderLeft}px`,
-                    borderColor: triggerStyles[0].borderColor,
-                    borderRadius: `${triggerStyles[0].borderRadius}px`,
-                    boxShadow: `${triggerStyles[0].triggerShadowHorizontal}px ${triggerStyles[0].triggerShadowVertical}px ${triggerStyles[0].triggerShadowBlur}px ${triggerStyles[0].triggerShadowColor} ${triggerStyles[0].triggerShadowPosition}`,
+                    borderStyle: triggerBorder.borderType,
+                    borderColor: triggerBorder.borderColor,
+                    boxShadow: `${triggerShadow.horizontal}px ${triggerShadow.vertical}px ${triggerShadow.blur}px ${triggerShadow.color} ${triggerShadow.position}`,
                 }}>
                     {triggerSettings[0].showIcon && triggerSettings[0].iconPosition == "before" && <i className={` premium-modal-box-icon ${triggerSettings[0].icon}`} style={{ fontSize: `${triggerSettings[0].iconSize}px`, marginRight: `${triggerSettings[0].iconSpacing}px`, color: triggerStyles[0].iconColor }}></i>}
-                    <span style={{ color: triggerStyles[0].color, fontFamily: triggerStyles[0].triggerFamily, fontWeight: triggerStyles[0].triggerWeight, fontStyle: triggerStyles[0].triggerStyle, letterSpacing: triggerStyles[0].triggerSpacing }}> {triggerSettings[0].btnText}</span>
+                    <span style={{
+                        color: triggerStyles[0].color,
+                        fontFamily: triggerTypography.fontFamily,
+                        fontWeight: triggerTypography.fontWeight,
+                        fontStyle: triggerTypography.fontStyle,
+                        letterSpacing: triggerTypography.letterSpacing
+                    }}> {triggerSettings[0].btnText}</span>
                     {triggerSettings[0].showIcon && triggerSettings[0].iconPosition == "after" && <i className={`premium-modal-box-icon ${triggerSettings[0].icon}`} style={{ fontSize: `${triggerSettings[0].iconSize}px`, marginLeft: `${triggerSettings[0].iconSpacing}px`, color: triggerStyles[0].iconColor }} ></i>}
                 </button>
                 }
                 {triggerSettings[0].triggerType === "image" && (
                     <img src={triggerSettings[0].triggerImgURL} className={`premium-modal-trigger-img`}
                         style={{
-                            borderStyle: triggerStyles[0].borderType,
-                            borderTopWidth: `${triggerBorderTop}px`,
-                            borderRightWidth: `${triggerBorderRight}px`,
-                            borderBottomWidth: `${triggerBorderBottom}px`,
-                            borderLeftWidth: `${triggerBorderLeft}px`,
-                            borderColor: triggerStyles[0].borderColor,
-                            borderRadius: `${triggerStyles[0].borderRadius}px`,
-                            boxShadow: `${triggerStyles[0].triggerShadowHorizontal}px ${triggerStyles[0].triggerShadowVertical}px ${triggerStyles[0].triggerShadowBlur}px ${triggerStyles[0].triggerShadowColor} ${triggerStyles[0].triggerShadowPosition}`,
+                            borderStyle: triggerBorder.borderType,
+                            borderColor: triggerBorder.borderColor,
+                            boxShadow: `${triggerShadow.horizontal}px ${triggerShadow.vertical}px ${triggerShadow.blur}px ${triggerShadow.color} ${triggerShadow.position}`,
                         }} />
                 )
                 }
                 {triggerSettings[0].triggerType === "text" && (
                     <span className={`premium-modal-trigger-text`} style={{
                         color: triggerStyles[0].color,
-                        borderStyle: triggerStyles[0].borderType,
-                        borderTopWidth: `${triggerBorderTop}px`,
-                        borderRightWidth: `${triggerBorderRight}px`,
-                        borderBottomWidth: `${triggerBorderBottom}px`,
-                        borderLeftWidth: `${triggerBorderLeft}px`,
-                        borderColor: triggerStyles[0].borderColor,
-                        borderRadius: `${triggerStyles[0].borderRadius}px`,
-                        textShadow: `${triggerStyles[0].textShadowHorizontal}px ${triggerStyles[0].textShadowVertical}px ${triggerStyles[0].textShadowBlur}px ${triggerStyles[0].textShadowColor}`,
-                        fontFamily: triggerStyles[0].triggerFamily,
-                        fontWeight: triggerStyles[0].triggerWeight,
-                        fontStyle: triggerStyles[0].triggerStyle,
-                        letterSpacing: triggerStyles[0].triggerSpacing
+                        borderStyle: triggerBorder.borderType,
+                        borderColor: triggerBorder.borderColor,
+                        textShadow: `${triggerTextShadow.horizontal}px ${triggerTextShadow.vertical}px ${triggerTextShadow.blur}px ${triggerTextShadow.color}`,
+                        fontFamily: triggerTypography.fontFamily,
+                        fontWeight: triggerTypography.fontWeight,
+                        fontStyle: triggerTypography.fontStyle,
+                        letterSpacing: triggerTypography.letterSpacing
                     }}>
                         {triggerSettings[0].triggerText}
                     </span>
@@ -155,12 +129,7 @@ const save = props => {
             </div>
             <div className="premium-popup__modal_wrap" style={{ display: "none" }} role="dialog">
                 <div role="presentation" className="premium-popup__modal_wrap_overlay" style={{
-                    backgroundColor: backgroundType === "solid" ? modalStyles[0].containerBack : '',
-                    backgroundImage: btnbg,
-                    backgroundRepeat: modalStyles[0].backgroundRepeat,
-                    backgroundPosition: modalStyles[0].backgroundPosition,
-                    backgroundSize: modalStyles[0].backgroundSize,
-                    backgroundAttachment: modalStyles[0].fixed ? "fixed" : "unset",
+                    ...gradientBackground(modalBackground)
                 }} >
                 </div>
                 <div className={`premium-popup__modal_content animated animation-${contentStyles[0].animationType} animation-${contentStyles[0].animationSpeed}`}
@@ -168,31 +137,21 @@ const save = props => {
                     data-animation={`${contentStyles[0].animationType} ${contentStyles[0].animationSpeed}`}
                     style={{
 
-                        borderStyle: `${modalStyles[0].borderType}`,
-                        borderColor: `${modalStyles[0].borderColor}`,
-                        borderTopWidth: `${modalBorderTop}px`,
-                        borderRightWidth: `${modalBorderRight}px`,
-                        borderBottomWidth: `${modalBorderBottom}px`,
-                        borderLeftWidth: `${modalBorderLeft}px`,
-                        borderRadius: `${modalStyles[0].borderRadius}px`,
-                        boxShadow: `${modalStyles[0].modalShadowHorizontal}px ${modalStyles[0].modalShadowVertical}px ${modalStyles[0].modalShadowBlur}px ${modalStyles[0].modalShadowColor} ${modalStyles[0].modalShadowPosition}`,
+                        borderStyle: `${modalBorder.borderType}`,
+                        borderColor: `${modalBorder.borderColor}`,
+                        boxShadow: `${modalShadow.horizontal}px ${modalShadow.vertical}px ${modalShadow.blur}px ${modalShadow.color} ${modalShadow.position}`,
                     }}>
                     {contentStyles[0].showHeader && <div className={`premium-modal-box-modal-header`} style={{
                         backgroundColor: headerStyles[0].backColor,
-                        borderStyle: headerStyles[0].borderType,
-                        borderTopWidth: `${headerBorderTop}px`,
-                        borderRightWidth: `${headerBorderRight}px`,
-                        borderBottomWidth: `${headerBorderBottom}px`,
-                        borderLeftWidth: `${headerBorderLeft}px`,
-                        borderColor: `${headerStyles[0].borderColor}`,
-                        borderRadius: `${headerStyles[0].borderRadius}px`,
+                        borderStyle: headerBorder.borderType,
+                        borderColor: `${headerBorder.borderColor}`,
                     }}>
                         <h3 className={`premium-modal-box-modal-title`} style={{
                             color: headerStyles[0].color,
-                            fontFamily: headerStyles[0].headerFamily,
-                            fontStyle: headerStyles[0].headerStyle,
-                            letterSpacing: headerStyles[0].headerSpacing,
-                            fontWeight: headerStyles[0].headerWeight,
+                            fontFamily: headerTypography.fontFamily,
+                            fontWeight: headerTypography.fontWeight,
+                            fontStyle: headerTypography.fontStyle,
+                            letterSpacing: headerTypography.letterSpacing
                         }}>
                             {contentStyles[0].iconType === "icon" && <i className={contentStyles[0].contentIcon}  ></i>}
                             {contentStyles[0].iconType === "image" && <img src={contentStyles[0].contentImgURL} style={{
@@ -210,13 +169,8 @@ const save = props => {
                         </h3>
                         {contentStyles[0].showUpperClose && contentStyles[0].showHeader && (<div className="premium-modal-box-close-button-container" style={{
                             backgroundColor: `${upperStyles[0].backColor}`,
-                            borderStyle: `${upperStyles[0].borderType}`,
-                            borderTopWidth: `${upperBorderTop}px`,
-                            borderRightWidth: `${upperBorderRight}px`,
-                            borderBottomWidth: `${upperBorderBottom}px`,
-                            borderLeftWidth: `${upperBorderLeft}px`,
-                            borderColor: `${upperStyles[0].borderColor}`,
-                            borderRadius: `${upperStyles[0].borderRadius}px`,
+                            borderStyle: `${upperBorder.borderType}`,
+                            borderColor: `${upperBorder.borderColor}`,
 
                         }}>
                             <button type="button" className="premium-modal-box-modal-close close-button"
@@ -232,10 +186,10 @@ const save = props => {
                     }}>
                         {modalStyles[0].contentType === "text" ? <p style={{
                             color: modalStyles[0].textColor,
-                            fontWeight: modalStyles[0].modalWeight,
-                            fontFamily: modalStyles[0].modalFamily,
-                            letterSpacing: modalStyles[0].modalSpacing,
-                            fontStyle: modalStyles[0].modalStyle,
+                            fontFamily: modalTypography.fontFamily,
+                            fontWeight: modalTypography.fontWeight,
+                            fontStyle: modalTypography.fontStyle,
+                            letterSpacing: modalTypography.letterSpacing
                         }} >{modalStyles[0].contentText}</p> : <InnerBlocks.Content />}
 
                     </div>
@@ -244,19 +198,14 @@ const save = props => {
                     }}>
                         <button className={`premium-modal-box-modal-lower-close close-button`} role="button" data-dismiss="premium-modal"
                             style={{
-                                fontStyle: lowerStyles[0].lowerStyle,
-                                fontWeight: lowerStyles[0].lowerWeight,
-                                letterSpacing: lowerStyles[0].lowerSpacing,
+                                fontWeight: lowerTypography.fontWeight,
+                                fontStyle: lowerTypography.fontStyle,
+                                letterSpacing: lowerTypography.letterSpacing,
                                 width: `${lowerStyles[0].iconWidth}${lowerStyles[0].iconWidthUnit}`,
                                 color: `${lowerStyles[0].color}`,
                                 backgroundColor: `${lowerStyles[0].backColor}`,
-                                borderStyle: `${lowerStyles[0].borderType}`,
-                                borderTopWidth: `${lowerBorderTop}px`,
-                                borderRightWidth: `${lowerBorderRight}px`,
-                                borderBottomWidth: `${lowerBorderBottom}px`,
-                                borderLeftWidth: `${lowerBorderLeft}px`,
-                                borderColor: `${lowerStyles[0].borderColor}`,
-                                borderRadius: `${lowerStyles[0].borderRadius}px`,
+                                borderStyle: `${lowerBorder.borderType}`,
+                                borderColor: `${lowerBorder.borderColor}`,
                             }}
                         >
                             {contentStyles[0].lowerCloseText}
