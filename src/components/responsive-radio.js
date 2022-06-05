73774c4fdaf5312ca21d27d88dfd2ebbd0f08224
salmamohamed74 <salma.mohamed@leap13.com>
2022-06-05 16:52:19 +0200

    create responsive radio component

---
+++ src/components/responsive-radio.js
@@ -0,0 +1,71 @@
+/**
+ * WordPress dependencies
+ */
+import { Button, ButtonGroup } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+/**
+ * Import Css
+ */
+import React, { useLayoutEffect, useState } from 'react';
+import { select } from '@wordpress/data';
+import RadioComponent from './radio-control';
+import Responsive from './responsive';
+
+const MultiButtonsControl = ({ choices, label, options, showIcons, onChange, value }) => {
+    const defaultValue = {
+        'Desktop': '',
+        'Tablet': '',
+        'Mobile': ''
+    }
+    const [state, setState] = useState(value ? value : defaultValue)
+    const [device, setDevice] = useState("Desktop");
+
+    const output = {};
+
+    const handleChange = (value, device) => {
+        const updatedState = { ...state };
+        updatedState[device] = value;
+        setState(updatedState)
+        onChange(updatedState)
+    }
+
+    output.Desktop = (
+        <RadioComponent
+            choices={choices}
+            showIcons={showIcons}
+            label={false}
+            onChange={(newValue) => handleChange(newValue, 'Desktop')}
+            value={state['Desktop']}
+        />
+    );
+    output.Tablet = (
+        <RadioComponent
+            choices={choices}
+            showIcons={showIcons}
+            label={false}
+            onChange={(newValue) => handleChange(newValue, 'Tablet')}
+            value={state['Tablet']}
+        />
+    );
+    output.Mobile = (
+        <RadioComponent
+            choices={choices}
+            showIcons={showIcons}
+            label={false}
+            onChange={(newValue) => handleChange(newValue, 'Mobile')}
+            value={state['Mobile']}
+        />
+    );
+
+    return (
+        <div className={`components-base-control premium-blocks-field `}>
+            <header>
+                {label && <span className="customize-control-title premium-control-title">{label}</span>}
+                <Responsive onChange={(newDevice) => setDevice(newDevice)} />
+            </header>
+            { output[device] ? output[device] : output.Desktop}
+        </div>
+    );
+};
+
+export default MultiButtonsControl;
