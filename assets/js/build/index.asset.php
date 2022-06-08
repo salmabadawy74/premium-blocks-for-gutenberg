26bb2d228a9bf959cd886536e15af35f0dfc5fc1
aaaharidy <abdelrhman@nilecreations.com>
2022-06-08 11:35:39 +0200

    accordion icon

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7b09f4e3dd1157314b25');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'fecde350cc0e354934c4');
--- src/blocks/accordion/index.js
+++ src/blocks/accordion/index.js
@@ -1,5 +1,5 @@
 import { accordion } from "../../../assets/js/settings";
-
+import PBG_Block_Icons from '../../../blocks-config/block-icons'
 import PbgIcon from "../icons";
 import edit from "./edit";
 import save from "./save";
@@ -13,7 +13,7 @@ const { registerBlockType } = wp.blocks;
 registerBlockType("premium/accordion", {
     title: __("Accordion"),
     description: __('Display collapsed content like FAQs using Premium Accordion Block.'),
-    icon: <PbgIcon icon="accordion" />,
+    icon: PBG_Block_Icons.accordion,
     category: "premium-blocks",
     attributes: attributes,
     supports: {
