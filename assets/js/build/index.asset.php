c580cbf7a5c23b3b9fc4ee2f4cb9e2bcdfa37dfd
salmamohamed74 <salma.mohamed@leap13.com>
2022-06-27 12:50:32 +0200

    add container block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '8c0880e0d21004ce320c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'a247521928230216e898');
