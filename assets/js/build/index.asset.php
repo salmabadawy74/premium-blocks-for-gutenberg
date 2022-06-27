3813f2bdc85ddbd61cb005efe932522059cc7a0a
salmamohamed74 <salma.mohamed@leap13.com>
2022-06-27 14:30:38 +0200

    add row block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'a247521928230216e898');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '6a0b9be84503453a386e');
