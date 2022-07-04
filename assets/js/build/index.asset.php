ea94ad6f51b8d653037f0d5766827ac8e168de35
salmamohamed74 <salma.mohamed@leap13.com>
2022-07-04 10:36:59 +0200

    apply changes to typography control

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '1016853214179badcda3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'c31bcc3a427cb9fb8d05');
