cd15dfae7e57486ab6616455683f0286db01cf35
salmamohamed74 <salma.mohamed@leap13.com>
2022-07-05 12:02:37 +0200

    works on beta branch

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '1016853214179badcda3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '510214b023fcbae9f955');
