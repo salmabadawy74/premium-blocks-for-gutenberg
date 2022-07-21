5192a46839d8d7f97b34f1b9a1cbf5cbe75ea3bf
Nermin <49684695+nerminleap13@users.noreply.github.com>
2022-07-21 21:31:02 +0200

    fancy2

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '07a8d5c6a40b6bf56fa0');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '52f83699f0b3cc2fb177');
