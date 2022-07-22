85ede778a53e66cbe497ff5e1e403502d866316a
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-07-22 12:39:26 +0200

    update  blocks

--- src/blocks/banner/index.js
+++ src/blocks/banner/index.js
@@ -7,20 +7,25 @@ import deprecatedContent from "./deprecated";
 import attributes from "./attributes";
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-import PBG_Block_Icons from '../../../blocks-config/block-icons'
+import PBG_Block_Icons from "../../../blocks-config/block-icons";
 
 registerBlockType("premium/banner", {
     title: __("Banner"),
-    description: __('Create attractive banners along with Heading, Description, and CTA using Premium Banner.'),
+    description: __(
+        "Create attractive banners along with Heading, Description, and CTA using Premium Banner."
+    ),
     icon: PBG_Block_Icons.banner,
     category: "premium-blocks",
     attributes: attributes,
-    keywords: [__('image', 'premium-blocks-for-gutenberg'), __('photo', 'premium-blocks-for-gutenberg')],
+    keywords: [
+        __("image", "premium-blocks-for-gutenberg"),
+        __("photo", "premium-blocks-for-gutenberg"),
+    ],
     supports: {
-        inserter: banner
+        inserter: banner,
     },
     example: {},
     edit: edit,
     save: save,
-    deprecated: deprecatedContent
+    deprecated: deprecatedContent,
 });
