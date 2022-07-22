6552942c0f7ebf680bcb45f99ce2481ffc21ef78
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-07-22 03:41:17 +0200

    updat fancy block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'e4d84c5c1f945becde02');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '769a2b6c192b2c748bad');
