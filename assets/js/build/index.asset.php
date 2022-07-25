f45932b96ba2fb10995d8911e4bfe14047499dec
aaaharidy <a.abdelhamed@leap13.com>
2022-07-25 12:49:52 +0200

    search input

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '85987d0dcd4c310c76eb');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '07f8ffbbebb495c042c3');
