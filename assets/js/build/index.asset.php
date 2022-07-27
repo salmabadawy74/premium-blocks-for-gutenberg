516ebc0ea9b344ae21abb55ba1f80e77f3949439
aaharidy <abdoharidy72@gmail.com>
2022-07-27 02:37:08 +0200

    testimonials, icon-box, accordion

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'bd26bc632bb262408a63');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'e4f64841ac3aa3f65ff8');
