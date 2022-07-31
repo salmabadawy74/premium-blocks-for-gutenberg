6de6401bebfc4ecf49ed9595248fef14a315950b
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-07-31 15:54:38 +0200

    apply changes to banner block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'd6cb6f546e4a1033fbf3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '4f9f01ac0476c4dac12c');
