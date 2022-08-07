e1185781f33d9571285579dd279b27ec530de43c
salmamohamed74 <salma.mohamed@leap13.com>
2022-08-07 10:32:09 +0200

    works on section block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '0138dc03dce0444686be');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '549951013311af6d978b');
