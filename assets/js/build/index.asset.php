4b944bbde468ccc6ca4ef400651aed8ad48771cd
Nermin <49684695+nerminleap13@users.noreply.github.com>
2022-08-11 14:48:07 +0200

    pricing table

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'b33ca5bd45730ff1e073');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'ecf21c1ae2a7c8a177bc');
