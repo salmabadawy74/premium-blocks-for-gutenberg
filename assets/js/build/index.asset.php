39325e22fc117c5ad52c7ef68316418b64c25064
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-08-14 09:45:09 +0200

    fix issues in modal styles

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '3db8a7a45c0fd8a24bfb');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '6330850c147baaffba86');
