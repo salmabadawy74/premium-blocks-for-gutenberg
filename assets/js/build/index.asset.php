48f7e2cdc9b9b52ca26ce3a153d7dba24fea030d
salmamohamed74 <salma.mohamed@leap13.com>
2022-08-16 16:07:46 +0200

    try condition statement

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => '9b564ffbd60fd090d965');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element'), 'version' => 'c48bc39bf91326c3c064');
