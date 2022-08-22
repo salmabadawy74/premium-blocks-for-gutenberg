be6c0fd3ab742a149374b90d46b8e5a128acc2cd
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-08-22 17:22:08 +0200

    convert class to function in testimonial

--- src/blocks/testimonials/index.js
+++ src/blocks/testimonials/index.js
@@ -3,20 +3,18 @@ import edit from "./edit";
 import save from "./save";
 import deprecatedContent from "./deprecated";
 import PBG_Block_Icons from '../../../blocks-config/block-icons'
-import testimonialsAttrs from "./attributes";
+import json from './block.json';
 
 const { __ } = wp.i18n;
 
 const { registerBlockType } = wp.blocks;
 
-
-
 registerBlockType("premium/testimonial", {
+    ...json,
     title: __("Testimonial"),
     icon: PBG_Block_Icons.testimonials,
     description: __("Proof your success by displaying customer reviews using Premium Testimonials Block."),
     category: "premium-blocks",
-    attributes: testimonialsAttrs,
     supports: {
         inserter: testimonial
     },
