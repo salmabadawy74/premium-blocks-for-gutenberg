59a8c3b825cd5d85c62bcbc7a641c027b47e97f0
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-08-23 15:13:01 +0200

    convert js to json in row

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '615640ee55af992e6086');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '137ee47222e89621374e');
