70e66e89b086e18c9a95edf8decdfe454579894d
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-08-24 12:59:22 +0200

    convert js to json and class to function in bullet-list

--- src/blocks/bullet-list/index.js
+++ src/blocks/bullet-list/index.js
@@ -1,27 +1,21 @@
-import {
-    bulletList
-} from "../../../assets/js/settings";
+import {bulletList} from "../../../assets/js/settings";
 
 import save from "./save";
 import edit from "./edit";
-import attributes from './attributes'
+import json from './block.json';
 import deprecated from './deprecated';
 import PBG_Block_Icons from '../../../blocks-config/block-icons'
 
-const {
-    __
-} = wp.i18n;
+const {__} = wp.i18n;
 
-const {
-    registerBlockType
-} = wp.blocks;
+const {registerBlockType} = wp.blocks;
 
 registerBlockType("premium/bullet-list", {
+    ...json,
     title: __("Bullet List"),
     description: __('Organize your website’s content with attractive lists using Premium Bullet List Block.', "premium-blocks-for-gutenberg"),
     icon: PBG_Block_Icons.bulletList,
     category: "premium-blocks",
-    attributes: attributes,
     supports: {
         inserter: bulletList
     },
