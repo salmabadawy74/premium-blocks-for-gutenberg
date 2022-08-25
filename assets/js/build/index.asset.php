84fbb4e1c9c7d8f0bd6a50e81f4037ea6185ac06
mernahanna <merna.hanna92@gmail.com>
2022-08-25 14:47:34 +0200

    fix issue in countup

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '37565f996c077eca790e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '04968248c7cea8ac21ac');
