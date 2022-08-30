6ecd6ff7b4b0ba8d4768d265fa5e80e3242f7313
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-08-30 09:12:18 +0200

    fix issue in blocks icon

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'd7773309496fec0e9c93');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '2b3ea2eb00e3b41626dc');
