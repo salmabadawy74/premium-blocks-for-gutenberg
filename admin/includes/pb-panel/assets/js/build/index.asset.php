517396c2a7397387967e36fd9af5f329cdb9cb5f
aaharidy <abdoharidy72@gmail.com>
2022-09-05 12:02:57 +0200

    mailchimp api

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e1701b0aca260d6840b8');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '27e4a0fe514326a8eccc');
