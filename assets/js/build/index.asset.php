70efc6330ab9f7d2e94db8c17ae6c44711a37af7
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-09-06 08:14:18 +0200

    fix style for shape divider

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'caf03c7aad5307a0ce6b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5bcd2333a22ec3e6fc31');
--- src/blocks/row/index.js
+++ src/blocks/row/index.js
@@ -1,21 +1,21 @@
-import json from './block.json';
-import edit from './edit';
+import json from "./block.json";
+import edit from "./edit";
 import Save from "./save";
-import variations from './variations';
+import variations from "./variations";
 import PBG_Block_Icons from "../../../blocks-config/block-icons";
-import { row } from '../../../assets/js/settings';
+import { row } from "../../../assets/js/settings";
 
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
 
 registerBlockType("premium/row", {
     ...json,
-    title: __("Container"),
+    title: __("Container", "premium-blocks-for-gutenberg"),
     icon: PBG_Block_Icons.row,
     category: "premium-blocks",
     supports: {
-        html: false,
-        inserter: row
+        inserter: row,
+        anchor: true,
     },
     variations,
     example: {},
