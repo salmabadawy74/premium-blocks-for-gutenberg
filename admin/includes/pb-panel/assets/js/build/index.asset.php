c4f500c30589373d39588411ae8b26a8f788f752
salmamohamed74 <salma.mohamed@leap13.com>
2022-09-07 14:17:12 +0200

    edit style for blocks

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '84ac0246eac5703dfb2f');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4124bb5a60edb9bc0501');
