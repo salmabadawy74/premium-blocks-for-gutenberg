e0eb5c230f90cc5339c9f873ff97e26bf7d8e2e2
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-09-08 19:18:33 +0200

    add  decativate button

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '30350057702df9528819');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '25c7e58fab2520dfdf22');
