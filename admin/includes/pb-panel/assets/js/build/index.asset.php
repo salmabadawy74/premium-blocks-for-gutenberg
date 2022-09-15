501837b532c0bbbf66a1409df02ee0002d4fe193
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-09-15 08:31:54 +0200

    fix error for sass files

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9399905b30849a804bdc');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '74bbe1a86c4a68aead48');
