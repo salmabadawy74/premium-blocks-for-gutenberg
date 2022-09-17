8398af13d8053765ba2fa943bc2d4cc741cb5f20
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-09-17 14:48:04 +0200

    dd node module to grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,50 @@
 module.exports = function (grunt) {
     "use strict";
 
-    const pkgInfo = grunt.file.readJSON('package.json');
-    const sass = require('node-sass');
+    const pkgInfo = grunt.file.readJSON("package.json");
+    const sass = require("node-sass");
 
-    const blocks = ['accordion', 'banner', 'breadcrumbs', 'bullet-list', 'button', 'content-switcher', 'count-up', 'dual-heading', 'fancy-text', 'heading', 'icon', 'icon-box', 'image-separator', 'lottie', 'maps', 'Modal', 'person', 'pricing-table', 'row', 'section', 'testimonials', 'trigger', 'video-box', 'search'];
+    const blocks = [
+        "accordion",
+        "banner",
+        "breadcrumbs",
+        "bullet-list",
+        "button",
+        "content-switcher",
+        "count-up",
+        "dual-heading",
+        "fancy-text",
+        "heading",
+        "icon",
+        "icon-box",
+        "image-separator",
+        "lottie",
+        "maps",
+        "Modal",
+        "person",
+        "pricing-table",
+        "row",
+        "section",
+        "testimonials",
+        "trigger",
+        "video-box",
+        "search",
+    ];
     const sassFiles = {};
-    const minifyFiles = {}
+    const minifyFiles = {};
 
-
-    blocks.map(block => sassFiles[`./assets/css/${block}.css`] = `./src/blocks/${block}/style.scss`);
-    blocks.map(block => minifyFiles[`./assets/css/minified/${block}.min.css`] = `./assets/css/${block}.css`);
+    blocks.map(
+        (block) =>
+            (sassFiles[
+                `./assets/css/${block}.css`
+            ] = `./src/blocks/${block}/style.scss`)
+    );
+    blocks.map(
+        (block) =>
+            (minifyFiles[
+                `./assets/css/minified/${block}.min.css`
+            ] = `./assets/css/${block}.css`)
+    );
 
     //Grunt Configuration
     grunt.initConfig({
@@ -19,10 +53,10 @@ module.exports = function (grunt) {
         bumpup: {
             options: {
                 updateProps: {
-                    pkg: 'package.json'
-                }
+                    pkg: "package.json",
+                },
             },
-            file: 'package.json'
+            file: "package.json",
         },
         sass: {
             options: {
@@ -32,41 +66,41 @@ module.exports = function (grunt) {
             dist: {
                 files: [
                     {
-                        "./assets/css/editorpanel.css": "./assets/sass/editorpanel.scss",
-
+                        "./assets/css/editorpanel.css":
+                            "./assets/sass/editorpanel.scss",
                     },
-                    sassFiles
-                ]
-            }
-
+                    sassFiles,
+                ],
+            },
         },
         cssmin: {
             css: {
                 files: [
                     {
-                        "./assets/css/minified/editorpanel.min.css": "./assets/css/editorpanel.css",
-
-                    }
-                    , minifyFiles]
+                        "./assets/css/minified/editorpanel.min.css":
+                            "./assets/css/editorpanel.css",
+                    },
+                    minifyFiles,
+                ],
             },
         },
         copy: {
             main: {
                 options: {
-                    mode: true
+                    mode: true,
                 },
                 src: [
                     "**",
                     "!nbproject/**",
-
                     "!vendor/**",
                     "!composer.json",
                     "!composer.lock",
-
                     "!.git/**",
                     "!*.sh",
                     "!*.zip",
-
+                    "!admin/includes/pb-panel/assests/js/node_modules/**",
+                    "!admin/includes/pb-panel/assests/js/package.json",
+                    "!admin/includes/pb-panel/assests/js/package-lock.json",
                     "!node_modules/**",
                     "!eslintrc.json",
                     "!.editorconfig",
@@ -87,24 +121,21 @@ module.exports = function (grunt) {
                     "!admin/includes/pb-panel/assets/js/src",
                     "!src/**",
                 ],
-                dest: "premium-blocks-for-gutenberg/"
-            }
+                dest: "premium-blocks-for-gutenberg/",
+            },
         },
         compress: {
             main: {
                 options: {
                     archive: "premium-blocks-for-gutenberg.zip",
-                    mode: "zip"
+                    mode: "zip",
                 },
                 files: [
                     {
-                        src: [
-                            "./premium-blocks-for-gutenberg/**"
-                        ]
-
-                    }
-                ]
-            }
+                        src: ["./premium-blocks-for-gutenberg/**"],
+                    },
+                ],
+            },
         },
         clean: {
             main: ["premium-blocks-for-gutenberg"],
@@ -113,47 +144,43 @@ module.exports = function (grunt) {
         wp_readme_to_markdown: {
             your_target: {
                 files: {
-                    "README.md": "readme.txt"
-                }
+                    "README.md": "readme.txt",
+                },
             },
         },
-
     });
 
-
     /* Read File Generation task */
-    grunt.loadNpmTasks("grunt-wp-readme-to-markdown")
-    grunt.loadNpmTasks('grunt-bumpup');
-    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks("grunt-wp-readme-to-markdown");
+    grunt.loadNpmTasks("grunt-bumpup");
+    grunt.loadNpmTasks("grunt-sass");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
-    grunt.loadNpmTasks("grunt-contrib-copy")
-    grunt.loadNpmTasks("grunt-contrib-compress")
-    grunt.loadNpmTasks("grunt-contrib-clean")
-
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
     // Run readme task
-    grunt.registerTask("readme", ["wp_readme_to_markdown"])
+    grunt.registerTask("readme", ["wp_readme_to_markdown"]);
     // SASS compile
     grunt.registerTask("default", ["sass", "cssmin:css"]);
     // min all
     grunt.registerTask("minify", ["cssmin:css"]);
 
-
     //Run bumpup, readme tasks
     grunt.registerTask("build", (releaseType) => {
+        releaseType = releaseType ? releaseType : "patch";
 
-        releaseType = releaseType ? releaseType : 'patch';
+        var prevStableVersion =
+            "patch" === releaseType
+                ? pkgInfo.prev_stable_version
+                : pkgInfo.version;
 
-        var prevStableVersion = 'patch' === releaseType ? pkgInfo.prev_stable_version : pkgInfo.version;
+        grunt.config.set("prev_stable_version", prevStableVersion);
 
-        grunt.config.set('prev_stable_version', prevStableVersion);
+        grunt.task.run("bumpup:" + releaseType);
 
-        grunt.task.run('bumpup:' + releaseType);
-
-        grunt.task.run('readme');
-
+        grunt.task.run("readme");
     });
-
 
     /* Run release tasks */
     grunt.registerTask("release", [
@@ -162,6 +189,4 @@ module.exports = function (grunt) {
         "compress",
         "clean:main",
     ]);
-
-
 };
