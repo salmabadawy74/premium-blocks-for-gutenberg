6cd9740fa020ba78c6bf81075291b999f7a225d2
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-10-02 15:14:14 +0200

    updated fonts for plugin icon in dashboard

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'a542cca8ecfeeedfe587');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7a6b65394dd0ddc1b527');
