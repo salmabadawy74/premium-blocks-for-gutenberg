3601c15db8b31865540e139288d0e08ef327fc75
mernahanna <merna.hanna92@gmail.com>
2022-10-10 18:03:18 +0200

    added save button to map api in panel

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7a6b65394dd0ddc1b527');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b9dd753083b20e18a174');
