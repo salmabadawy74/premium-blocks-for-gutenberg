c12712a8f7eec8e54c58f40bc3cf7489b1d7b8ab
aaaharidy <abdelrhman@nilecreations.com>
2022-10-11 16:07:50 +0200

    css generator error

--- classes/class-pbg-style-generator.php
+++ classes/class-pbg-style-generator.php
@@ -79,17 +79,13 @@ if ( ! class_exists( 'Pbg_Style_Generator' ) ) {
 			if ( $files_count > 0 ) {
 				$merged_file = '';
 				foreach ( $css_files as $k => $file ) {
-					if ( ini_get( 'allow_url_fopen' ) ) {
-						$merged_style .= file_get_contents( $file );
-					} else {
-						$merged_style .= self::file_get_contents( $file );
-					}
 					require_once ABSPATH . 'wp-admin/includes/file.php'; // We will probably need to load this file.
 					global $wp_filesystem;
+					WP_Filesystem(); // Initial WP file system.
+					$merged_style .= $wp_filesystem->get_contents( PREMIUM_BLOCKS_PATH . $file );
+
 					$upload_dir = wp_upload_dir(); // Grab uploads folder array.
 					$dir        = trailingslashit( $upload_dir['basedir'] ) . 'premium-blocks-for-gutenberg/'; // Set storage directory path.
-
-					WP_Filesystem(); // Initial WP file system.
 					$wp_filesystem->mkdir( $dir ); // Make a new folder for storing our file.
 					$wp_filesystem->put_contents( $dir . 'style.css', $merged_style, 0777 | 0644 ); // Finally, store the file :D.
 					$wp_upload_dir = $upload_dir['baseurl'] . '/' . 'premium-blocks-for-gutenberg/';
@@ -100,29 +96,6 @@ if ( ! class_exists( 'Pbg_Style_Generator' ) ) {
 				return false;
 			}
 
-		}
-
-		/**
-		 * Get File Content.
-		 *
-		 * @param string $path file path.
-		 *
-		 * @return string
-		 */
-		public static function file_get_contents( $url ) {
-			$ch = curl_init();
-
-			curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-			curl_setopt( $ch, CURLOPT_HEADER, 0 );
-			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-			curl_setopt( $ch, CURLOPT_URL, $url );
-			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-			curl_setopt( $ch, CURLOPT_TIMEOUT, 10 );
-
-			$data = curl_exec( $ch );
-			curl_close( $ch );
-
-			return $data;
 		}
 
 		/**
