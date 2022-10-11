cc929a9b8c8b2501d084b62ac77252dc4dd91842
mernahanna <merna.hanna92@gmail.com>
2022-10-11 10:19:16 +0200

    change name of data in setting tab in panel

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b9dd753083b20e18a174');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '86c2760807b6039a27db');
