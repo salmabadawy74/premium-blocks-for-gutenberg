13d99feb632a36d046c814d7cd682ddc553e1181
aaaharidy <abdelrhman@nilecreations.com>
2022-10-30 16:50:29 +0200

    path

--- .history/classes/class-pbg-blocks-helper_20221027151157.php
+++ .history/classes/class-pbg-blocks-helper_20221027151157.php
@@ -158,7 +158,7 @@ class PBG_Blocks_Helper {
 	 * @return void
 	 */
 	public function add_blocks_editor_styles() {
-		Pbg_Style_Generator::pbg_add_css( PREMIUM_BLOCKS_PATH . 'assets/css/minified/blockseditor.min.css' );
+		Pbg_Style_Generator::pbg_add_css( 'assets/css/minified/blockseditor.min.css' );
 		if ( is_array( self::$blocks ) && ! empty( self::$blocks ) ) {
 			foreach ( self::$blocks as $slug => $value ) {
 
@@ -166,7 +166,7 @@ class PBG_Blocks_Helper {
 					continue;
 				}
 
-				Pbg_Style_Generator::pbg_add_css( PREMIUM_BLOCKS_PATH . "assets/css/minified/{$slug}.min.css" );
+				Pbg_Style_Generator::pbg_add_css( "assets/css/minified/{$slug}.min.css" );
 			}
 		}
 	}
