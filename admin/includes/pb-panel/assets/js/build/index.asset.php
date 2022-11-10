3b0d6feac78d49a32c078959a72ba06cbb2cf84a
aaaharidy <abdelrhman@nilecreations.com>
2022-11-10 13:28:31 +0200

    preview link and rollback version

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c6cd6081be95e9c008d6');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b2fc6e3493973a4c8d6c');
--- premium-blocks-for-gutenberg.php
+++ premium-blocks-for-gutenberg.php
@@ -25,7 +25,7 @@ define( 'PREMIUM_BLOCKS_URL', plugins_url( '/', __FILE__ ) );
 define( 'PREMIUM_BLOCKS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'PREMIUM_BLOCKS_FILE', __FILE__ );
 define( 'PREMIUM_BLOCKS_BASENAME', plugin_basename( __FILE__ ) );
-define( 'PREMIUM_BLOCKS_STABLE_VERSION', '1.9.7' );
+define( 'PREMIUM_BLOCKS_STABLE_VERSION', '1.9.9' );
 define( 'PBG_TABLET_BREAKPOINT', '976' );
 define( 'PBG_MOBILE_BREAKPOINT', '767' );
 
