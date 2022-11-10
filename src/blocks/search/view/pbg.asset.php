f87e1bda309d3accd8c17b3d6b8097c3a8e05958
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-11-10 07:02:31 +0200

    edit inspector in vedio box

---
+++ src/blocks/search/view/pbg.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '9c0d48b00735f4565f89');
--- src/blocks/search/view/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'df931843beb178ae4be0');
