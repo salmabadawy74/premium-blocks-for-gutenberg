cb47d5d94370cb005dc4fbf616322272fc531fae
aaaharidy <abdelrhman@nilecreations.com>
2022-11-23 14:41:10 +0200

    headings style and pot after build

--- admin/includes/pb-panel/assets/js/build/index.asset.php
+++ admin/includes/pb-panel/assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '598bec806ea6ffdfd7d2');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c19f692aadf4e1095cbc');
