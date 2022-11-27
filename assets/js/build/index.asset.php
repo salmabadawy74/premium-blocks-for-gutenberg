3e3caad0f5cc441ba21e140c7396c89e8944a94c
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-11-27 10:20:09 +0200

    fixed style issues in bullet list

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1116b5f1321bb697f8a9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '16b75000c754bf25ef00');
