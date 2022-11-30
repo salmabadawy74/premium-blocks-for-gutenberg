c3d495c14e94377f7d8541a520cd0826e8a8bb9e
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-11-30 19:42:18 +0200

    add style to svg

--- src/blocks/button/presets.js
+++ src/blocks/button/presets.js
@@ -56,7 +56,7 @@ export const buttonsPresets = [
                 },
             },
         ],
-        icon: `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>fill-color</title><path id="Fill_Color" data-name="Fill Color" class="cls-1" d="M0,0V37H103V0ZM75,20H27.93V17H75Z"/></svg>`,
+        icon: `<svg id="Layer_1" class="presets__btn_fill" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>fill-color</title><path id="Fill_Color" data-name="Fill Color" class="cls-1" d="M0,0V37H103V0ZM75,20H27.93V17H75Z"/></svg>`,
     },
     {
         value: "preset-2",
@@ -133,7 +133,7 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>outline</title><path id="Outline" class="cls-1" d="M102,1V36H1V1H102m1-1H0V37H103V0ZM75,20V17H28.06v3Z"/></svg>',
+            '<svg id="Layer_1" class="presets__btn_outline" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>outline</title><path id="Outline" class="cls-1" d="M102,1V36H1V1H102m1-1H0V37H103V0ZM75,20V17H28.06v3Z"/></svg>',
     },
     {
         value: "preset-3",
@@ -184,7 +184,7 @@ export const buttonsPresets = [
                 },
             },
         ],
-        icon: `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>fill-with-icon</title><g id="Fill_With_Icon" data-name="Fill With Icon"><path class="cls-1" d="M0,0V37H103V0ZM69,20H22V17H69ZM79.08,22,76.52,20.6,74,22l.48-2.86-2.07-2,2.87-.41,1.28-2.6,1.28,2.6,2.87.41-2.08,2Z"/></g></svg>`,
+        icon: `<svg id="Layer_1" class="presets__btn_fill" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>fill-with-icon</title><g id="Fill_With_Icon" data-name="Fill With Icon"><path class="cls-1" d="M0,0V37H103V0ZM69,20H22V17H69ZM79.08,22,76.52,20.6,74,22l.48-2.86-2.07-2,2.87-.41,1.28-2.6,1.28,2.6,2.87.41-2.08,2Z"/></g></svg>`,
     },
     {
         value: "preset-4",
@@ -268,7 +268,7 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>outline-with-icon</title><g id="Outline_With_Icon" data-name="Outline With Icon"><path class="cls-1" d="M102,1V36H1V1H102m1-1H0V37H103V0Z"/><rect class="cls-1" x="44" y="-5" width="3" height="47" transform="translate(64 -27) rotate(90)"/><polygon class="cls-1" points="76.52 14.06 77.8 16.66 80.67 17.07 78.59 19.09 79.08 21.95 76.52 20.6 73.95 21.95 74.44 19.09 72.37 17.07 75.24 16.66 76.52 14.06"/></g></svg>',
+            '<svg id="Layer_1" class="presets__btn_outline" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>outline-with-icon</title><g id="Outline_With_Icon" data-name="Outline With Icon"><path class="cls-1" d="M102,1V36H1V1H102m1-1H0V37H103V0Z"/><rect class="cls-1" x="44" y="-5" width="3" height="47" transform="translate(64 -27) rotate(90)"/><polygon class="cls-1" points="76.52 14.06 77.8 16.66 80.67 17.07 78.59 19.09 79.08 21.95 76.52 20.6 73.95 21.95 74.44 19.09 72.37 17.07 75.24 16.66 76.52 14.06"/></g></svg>',
     },
     {
         value: "preset-5",
@@ -363,7 +363,7 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>rounded-fill</title><path id="Rounded_Fill" data-name="Rounded Fill" class="cls-1" d="M96,0H7A7,7,0,0,0,0,7V30a7,7,0,0,0,7,7H96a7,7,0,0,0,7-7V7A7,7,0,0,0,96,0ZM75,20H28V17H75Z"/></svg>',
+            '<svg id="Layer_1" class="presets__btn_fill" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>rounded-fill</title><path id="Rounded_Fill" data-name="Rounded Fill" class="cls-1" d="M96,0H7A7,7,0,0,0,0,7V30a7,7,0,0,0,7,7H96a7,7,0,0,0,7-7V7A7,7,0,0,0,96,0ZM75,20H28V17H75Z"/></svg>',
     },
     {
         value: "preset-6",
@@ -440,7 +440,7 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>rounded-outline</title><g id="Rounded_Outline" data-name="Rounded Outline"><path class="cls-1" d="M96,1a6,6,0,0,1,6,6V30a6,6,0,0,1-6,6H7a6,6,0,0,1-6-6V7A6,6,0,0,1,7,1H96m0-1H7A7,7,0,0,0,0,7V30a7,7,0,0,0,7,7H96a7,7,0,0,0,7-7V7a7,7,0,0,0-7-7Z"/><rect class="cls-1" x="50" y="-5" width="3" height="47" transform="translate(70 -33) rotate(90)"/></g></svg>',
+            '<svg id="Layer_1" class="presets__btn_outline" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>rounded-outline</title><g id="Rounded_Outline" data-name="Rounded Outline"><path class="cls-1" d="M96,1a6,6,0,0,1,6,6V30a6,6,0,0,1-6,6H7a6,6,0,0,1-6-6V7A6,6,0,0,1,7,1H96m0-1H7A7,7,0,0,0,0,7V30a7,7,0,0,0,7,7H96a7,7,0,0,0,7-7V7a7,7,0,0,0-7-7Z"/><rect class="cls-1" x="50" y="-5" width="3" height="47" transform="translate(70 -33) rotate(90)"/></g></svg>',
     },
     {
         value: "preset-7",
@@ -535,7 +535,7 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>circled-fill</title><g id="Circled_Fill" data-name="Circled Fill"><path class="cls-1" d="M84.5,0h-66a18.5,18.5,0,0,0,0,37h66a18.5,18.5,0,0,0,0-37ZM75,20H28V17H75Z"/></g></svg>',
+            '<svg id="Layer_1" class="presets__btn_fill" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>circled-fill</title><g id="Circled_Fill" data-name="Circled Fill"><path class="cls-1" d="M84.5,0h-66a18.5,18.5,0,0,0,0,37h66a18.5,18.5,0,0,0,0-37ZM75,20H28V17H75Z"/></g></svg>',
     },
     {
         value: "preset-8",
@@ -612,6 +612,6 @@ export const buttonsPresets = [
             },
         ],
         icon:
-            '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>circled-outline</title><g id="Circled_Outline" data-name="Circled Outline"><path class="cls-1" d="M84.5,1a17.5,17.5,0,0,1,0,35h-66a17.5,17.5,0,0,1,0-35h66m0-1h-66A18.5,18.5,0,0,0,0,18.5H0A18.5,18.5,0,0,0,18.5,37h66A18.5,18.5,0,0,0,103,18.5h0A18.5,18.5,0,0,0,84.5,0Z"/><rect class="cls-1" x="50" y="-5" width="3" height="47" transform="translate(70 -33) rotate(90)"/></g></svg>',
+            '<svg id="Layer_1" class="presets__btn_outline" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" width="103" height="37" viewBox="0 0 103 37"><defs></defs><title>circled-outline</title><g id="Circled_Outline" data-name="Circled Outline"><path class="cls-1" d="M84.5,1a17.5,17.5,0,0,1,0,35h-66a17.5,17.5,0,0,1,0-35h66m0-1h-66A18.5,18.5,0,0,0,0,18.5H0A18.5,18.5,0,0,0,18.5,37h66A18.5,18.5,0,0,0,103,18.5h0A18.5,18.5,0,0,0,84.5,0Z"/><rect class="cls-1" x="50" y="-5" width="3" height="47" transform="translate(70 -33) rotate(90)"/></g></svg>',
     },
 ];
