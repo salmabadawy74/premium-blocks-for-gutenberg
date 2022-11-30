2cf210807ec48cd94d23b9742f7fe09c53e65ce6
mernahanna <merna.hanna92@gmail.com>
2022-11-30 18:42:40 +0200

    added spacing between accordion item in accordion

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c6591e0412ef79c47259');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '40a7964921b0488dd0c6');
