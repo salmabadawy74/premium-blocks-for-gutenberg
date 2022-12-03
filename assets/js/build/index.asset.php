8783ddbc467c8bd40c2699549ec60d89bd2f8658
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-03 22:09:13 +0200

    fix style for presets in button

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5da9a1baedddba5f849f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b68f0ec3782bf620550b');
