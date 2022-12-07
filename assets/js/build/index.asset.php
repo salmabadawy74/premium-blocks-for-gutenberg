4f7d05a9b4180f9be33cfbd849a465e31ec23b77
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-07 11:48:36 +0200

    fix impoert json file

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '989915bb40bc06519a9a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f0ca79177a57bac845db');
