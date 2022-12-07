6f638927ba141f641d553c4dbd9c750eeb2f0057
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-07 18:35:32 +0200

    fix style for container

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fc6bcd579e8f404ff723');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '98fb2e64cb1e6631461d');
