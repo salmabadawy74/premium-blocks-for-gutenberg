b02e982848e9ac2e9832dfc9388ad61af3eefdee
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-12 16:18:03 +0200

    create post title block

---
+++ src/blocks/post-title/save.js
@@ -0,0 +1,10 @@
+import { __ } from "@wordpress/i18n";
+export default function save() {
+    return (
+        <h3 className="">
+            <a href="/" target="_blank" rel="noopener noreferrer" alt="">
+                {" "}
+            </a>
+        </h3>
+    );
+}
