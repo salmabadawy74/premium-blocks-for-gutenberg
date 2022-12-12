4272bd3e52357d5920b171b3d425477336766cfb
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-12 18:17:54 +0200

    update google fonts

--- assets/js/build/pbg.asset.php
+++ assets/js/build/pbg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '00a6b6064ddc9d382b52');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '4d475cfc9215459b9cca');
