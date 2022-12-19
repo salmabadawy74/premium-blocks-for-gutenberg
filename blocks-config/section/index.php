8f5740d76b8ecc73e9f87bc80a478b873817c48c
salmamohamed74 <sos.mohamed93@yahoo.com>
2022-12-19 10:28:15 +0200

    add js file

--- blocks-config/section/index.php
+++ blocks-config/section/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Server-side rendering of the `pbg/section` block.
  *
@@ -15,95 +16,96 @@
  * @param string $attr option attribute.
  * @param string $unique_id option For block ID.
  */
-function get_premium_section_css_style( $attr, $unique_id ) {
+function get_premium_section_css_style($attr, $unique_id)
+{
 	$css                    = new Premium_Blocks_css();
 	$media_query            = array();
-	$media_query['mobile']  = apply_filters( 'Premium_BLocks_mobile_media_query', '(max-width: 767px)' );
-	$media_query['tablet']  = apply_filters( 'Premium_BLocks_tablet_media_query', '(max-width: 1024px)' );
-	$media_query['desktop'] = apply_filters( 'Premium_BLocks_tablet_media_query', '(min-width: 1025px)' );
+	$media_query['mobile']  = apply_filters('Premium_BLocks_mobile_media_query', '(max-width: 767px)');
+	$media_query['tablet']  = apply_filters('Premium_BLocks_tablet_media_query', '(max-width: 1024px)');
+	$media_query['desktop'] = apply_filters('Premium_BLocks_tablet_media_query', '(min-width: 1025px)');
 
-	if ( isset( $attr['padding'] ) ) {
+	if (isset($attr['padding'])) {
 		$padding = $attr['padding'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'padding', $css->render_spacing( $padding['Desktop'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('padding', $css->render_spacing($padding['Desktop'], $padding['unit']));
 	}
 
-	if ( isset( $attr['margin'] ) ) {
+	if (isset($attr['margin'])) {
 		$padding = $attr['margin'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'margin', $css->render_spacing( $padding['Desktop'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('margin', $css->render_spacing($padding['Desktop'], $padding['unit']));
 	}
 
-	if ( isset( $attr['border'] ) ) {
+	if (isset($attr['border'])) {
 		$border        = $attr['border'];
 		$border_width  = $border['borderWidth'];
 		$border_radius = $border['borderRadius'];
 
-		$css->set_selector( $unique_id );
-		$css->add_property( 'border-width', $css->render_spacing( $border_width['Desktop'], 'px' ) );
-		$css->add_property( 'border-radius', $css->render_spacing( $border_radius['Desktop'], 'px' ) );
+		$css->set_selector($unique_id);
+		$css->add_property('border-width', $css->render_spacing($border_width['Desktop'], 'px'));
+		$css->add_property('border-radius', $css->render_spacing($border_radius['Desktop'], 'px'));
 	}
-	if ( isset( $attr['horAlign'] ) ) {
-		$css->set_selector( $unique_id );
-		$css->add_property( 'text-align', $css->get_responsive_css( $attr['horAlign'], 'Desktop' ) );
+	if (isset($attr['horAlign'])) {
+		$css->set_selector($unique_id);
+		$css->add_property('text-align', $css->get_responsive_css($attr['horAlign'], 'Desktop'));
 	}
-	$css->start_media_query( $media_query['tablet'] );
+	$css->start_media_query($media_query['tablet']);
 
-	if ( isset( $attr['padding'] ) ) {
+	if (isset($attr['padding'])) {
 		$padding = $attr['padding'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'padding', $css->render_spacing( $padding['Tablet'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('padding', $css->render_spacing($padding['Tablet'], $padding['unit']));
 	}
 
-	if ( isset( $attr['margin'] ) ) {
+	if (isset($attr['margin'])) {
 		$padding = $attr['margin'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'margin', $css->render_spacing( $padding['Tablet'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('margin', $css->render_spacing($padding['Tablet'], $padding['unit']));
 	}
 
-	if ( isset( $attr['border'] ) ) {
+	if (isset($attr['border'])) {
 		$border        = $attr['border'];
 		$border_width  = $border['borderWidth'];
 		$border_radius = $border['borderRadius'];
 
-		$css->set_selector( $unique_id );
-		$css->add_property( 'border-width', $css->render_spacing( $border_width['Tablet'], 'px' ) );
-		$css->add_property( 'border-radius', $css->render_spacing( $border_radius['Tablet'], 'px' ) );
+		$css->set_selector($unique_id);
+		$css->add_property('border-width', $css->render_spacing($border_width['Tablet'], 'px'));
+		$css->add_property('border-radius', $css->render_spacing($border_radius['Tablet'], 'px'));
 	}
 
-	if ( isset( $attr['horAlign'] ) ) {
-		$css->set_selector( $unique_id );
-		$css->add_property( 'text-align', $css->get_responsive_css( $attr['horAlign'], 'Tablet' ) );
+	if (isset($attr['horAlign'])) {
+		$css->set_selector($unique_id);
+		$css->add_property('text-align', $css->get_responsive_css($attr['horAlign'], 'Tablet'));
 	}
 	$css->stop_media_query();
 
-	$css->start_media_query( $media_query['mobile'] );
+	$css->start_media_query($media_query['mobile']);
 
-	if ( isset( $attr['padding'] ) ) {
+	if (isset($attr['padding'])) {
 		$padding = $attr['padding'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'padding', $css->render_spacing( $padding['Mobile'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('padding', $css->render_spacing($padding['Mobile'], $padding['unit']));
 	}
 
-	if ( isset( $attr['margin'] ) ) {
+	if (isset($attr['margin'])) {
 		$padding = $attr['margin'];
-		$css->set_selector( $unique_id );
-		$css->add_property( 'margin', $css->render_spacing( $padding['Mobile'], $padding['unit'] ) );
+		$css->set_selector($unique_id);
+		$css->add_property('margin', $css->render_spacing($padding['Mobile'], $padding['unit']));
 	}
 
-	if ( isset( $attr['border'] ) ) {
+	if (isset($attr['border'])) {
 		$border        = $attr['border'];
 		$border_width  = $border['borderWidth'];
 		$border_radius = $border['borderRadius'];
 
-		$css->set_selector( $unique_id );
-		$css->add_property( 'border-width', $css->render_spacing( $border_width['Mobile'], 'px' ) );
-		$css->add_property( 'border-radius', $css->render_spacing( $border_radius['Mobile'], 'px' ) );
+		$css->set_selector($unique_id);
+		$css->add_property('border-width', $css->render_spacing($border_width['Mobile'], 'px'));
+		$css->add_property('border-radius', $css->render_spacing($border_radius['Mobile'], 'px'));
 	}
 
-	if ( isset( $attr['horAlign'] ) ) {
-		$css->set_selector( $unique_id );
-		$css->add_property( 'text-align', $css->get_responsive_css( $attr['horAlign'], 'Mobile' ) );
+	if (isset($attr['horAlign'])) {
+		$css->set_selector($unique_id);
+		$css->add_property('text-align', $css->get_responsive_css($attr['horAlign'], 'Mobile'));
 	}
 	$css->stop_media_query();
 	return $css->css_output();
@@ -118,13 +120,15 @@ function get_premium_section_css_style( $attr, $unique_id ) {
  *
  * @return string Returns the post content with the legacy widget added.
  */
-function render_block_pbg_section( $attributes, $content, $block ) {
+function render_block_pbg_section($attributes, $content, $block)
+{
+	$is_rtl = is_rtl() ? true : false;
 	$block_helpers = pbg_blocks_helper();
-	if ( isset( $attributes['block_id'] ) && ! empty( $attributes['block_id'] ) ) {
+	if (isset($attributes['block_id']) && !empty($attributes['block_id'])) {
 		$unique_id = '.premium-container';
 	}
 
-	if ( isset( $attributes['blockId'] ) && ! empty( $attributes['blockId'] ) ) {
+	if (isset($attributes['blockId']) && !empty($attributes['blockId'])) {
 		$unique_id = ".{$attributes['blockId']}";
 	}
 	wp_enqueue_style(
@@ -134,16 +138,32 @@ function render_block_pbg_section( $attributes, $content, $block ) {
 		PREMIUM_BLOCKS_VERSION,
 		'all'
 	);
-	$style_id = 'pbg-blocks-style' . esc_attr( $unique_id );
-	if ( ! wp_style_is( $style_id, 'enqueued' ) && apply_filters( 'Premium_BLocks_blocks_render_inline_css', true, 'section', $unique_id ) ) {
+	wp_enqueue_script(
+		'pbg-section',
+		PREMIUM_BLOCKS_URL . 'assets/js/section.js',
+		array('jquery'),
+		PREMIUM_BLOCKS_VERSION,
+		true
+	);
+
+	wp_localize_script(
+		'pbg-section',
+		'siteDirection',
+		array(
+			'isRTL' => $is_rtl,
+		)
+	);
+
+	$style_id = 'pbg-blocks-style' . esc_attr($unique_id);
+	if (!wp_style_is($style_id, 'enqueued') && apply_filters('Premium_BLocks_blocks_render_inline_css', true, 'section', $unique_id)) {
 		// If filter didn't run in header (which would have enqueued the specific css id ) then filter attributes for easier dynamic css.
 		// $attributes = apply_filters( 'Premium_BLocks_blocks_column_render_block_attributes', $attributes );
-		$css = get_premium_section_css_style( $attributes, $unique_id );
-		if ( ! empty( $css ) ) {
-			if ( $block_helpers->should_render_inline( 'section', $unique_id ) ) {
-				$content = '<style id="pbg-blocks-style' . esc_attr( $unique_id ) . '">' . $css . '</style>' . $content;
+		$css = get_premium_section_css_style($attributes, $unique_id);
+		if (!empty($css)) {
+			if ($block_helpers->should_render_inline('section', $unique_id)) {
+				$content = '<style id="pbg-blocks-style' . esc_attr($unique_id) . '">' . $css . '</style>' . $content;
 			} else {
-				$block_helpers->render_inline_css( $css, $style_id, true );
+				$block_helpers->render_inline_css($css, $style_id, true);
 			}
 		}
 	};
@@ -160,8 +180,9 @@ function render_block_pbg_section( $attributes, $content, $block ) {
  * @uses render_block_pbg_section()
  * @throws WP_Error An WP_Error exception parsing the block definition.
  */
-function register_block_pbg_section() {
-	if ( ! function_exists( 'register_block_type' ) ) {
+function register_block_pbg_section()
+{
+	if (!function_exists('register_block_type')) {
 		return;
 	}
 	register_block_type(
