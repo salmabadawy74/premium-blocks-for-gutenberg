c2d436dfc72f803337df6687aa5704d372f2c6fb
mernahanna <merna.hanna92@gmail.com>
2022-12-22 09:58:29 +0200

    fixed issue in dual heading deprecated

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '155ed50040cb16a3ecca');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '73e10018bddcdaa6543e');
