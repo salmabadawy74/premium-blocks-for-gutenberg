9b9c0f1350dbb6dbf7c57bcfecae3c72f7151f5c
Nermin <49684695+nerminleap13@users.noreply.github.com>
2022-12-27 12:36:12 +0200

    show hover effect with icon/image icon block

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dd29df0ca2d1a2f5894c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c50872ae1dee3929db07');
