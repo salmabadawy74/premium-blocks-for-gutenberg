66ec623e73afe5efffd0d3104ffe57a28ea5365d
Nermin <49684695+nerminleap13@users.noreply.github.com>
2022-12-27 13:20:14 +0200

    fix icon group hover , icon sizes

--- assets/js/build/index.asset.php
+++ assets/js/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c50872ae1dee3929db07');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a01c2936037208994d54');
