db66b0eafa56e8decf9ba869c1454beacaa8cfc4
Nile Creations <nilecreations@Niles-Mac-mini.local>
2022-12-28 09:32:37 +0200

    added attributes to deprecated in icon

--- assets/js/build/pbg.asset.php
+++ assets/js/build/pbg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'e1fd45c0d9be4e2a3994');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '33834ebc695c6aac1755');
